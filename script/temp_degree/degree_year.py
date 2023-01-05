9506ee4468eea5af4883ce680d772ba106a6816f
Quan <quan.liu@mpimet.mpg.de>
2023-01-05 17:53:57 +0100

    ing

--- script/temp_degree/degree_year.py
+++ script/temp_degree/degree_year.py
@@ -1,16 +1,18 @@
 import xarray as xr
 import numpy as np
 
+
 def return_year(xarr):
     return xarr.time.dt.year.values
 
-def degree_year(fldmean:xr.DataArray):
+
+def degree_year(fldmean: xr.DataArray):
     """
     to calculate the year when the mean global surface temperature reaches 1,2,and 4 degrees.
     **Argument**
         *fldmean* the fldmean of tsurf
     """
-    if isinstance(fldmean,xr.DataArray):
+    if isinstance(fldmean, xr.DataArray):
         pass
     else:
         print("only DataArray is accapted, DataSet recevied")
@@ -22,7 +24,7 @@ def degree_year(fldmean:xr.DataArray):
 
     # ens mean
     if fldmean.ens.size != 1:
-        fld_ens_mean = fldmean.mean(dim = 'ens')
+        fld_ens_mean = fldmean.mean(dim="ens")
     else:
         fld_ens_mean = fldmean
 
@@ -30,7 +32,7 @@ def degree_year(fldmean:xr.DataArray):
     mean = fld_ens_mean.squeeze()
 
     # anomaly
-    anomaly = mean-mean[0]
+    anomaly = mean - mean[0]
 
     years = np.zeros(3)
 
@@ -38,9 +40,9 @@ def degree_year(fldmean:xr.DataArray):
     years[0] = return_year(anomaly[5])
 
     # 2 degree
-    years[1] = return_year(anomaly.where(anomaly>=2,drop=True)).squeeze()[0]
+    years[1] = return_year(anomaly.where(anomaly >= 2, drop=True)).squeeze()[0]
 
     # 4 degree
-    years[2] = return_year(anomaly.where(anomaly>=4,drop=True)).squeeze()[0]
+    years[2] = return_year(anomaly.where(anomaly >= 4, drop=True)).squeeze()[0]
 
     return years
