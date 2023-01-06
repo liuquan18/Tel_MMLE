1b00c4d1b116803bd2551debc0adc38cbcb275fe
Quan <quan.liu@mpimet.mpg.de>
2023-01-06 16:02:28 +0100

    keys

--- src/plots/plot_violin.py
+++ src/plots/plot_violin.py
@@ -16,21 +16,23 @@ def to_dataframe(period, mode, name):
     return period
 
 
-def xr2df(first10_all_period, last10_all_period, mode):
+def xr2df(first10_all_period, last10_all_period, mode, compare):
     """
     prepare dataframe for seaborn.
     """
     first = to_dataframe(first10_all_period, mode=mode, name=mode)
     last = to_dataframe(last10_all_period, mode=mode, name=mode)
-    periods = pd.concat(
-        [first, last], keys=["first10", "last10"], names=["period", "hlayers", "com"]
-    )
+    if compare == "CO2":
+        keys = ["first10", "last10"]
+    elif compare == "temp":
+        keys = ["0C", "4C"]
+    periods = pd.concat([first, last], keys=keys, names=["period", "hlayers", "com"])
     periods = periods.reset_index().set_index("com")
     periods["hlayers"] = periods["hlayers"] / 100
     return periods
 
 
-def plot_vilion(first, last, std_type, split=False):
+def plot_vilion(first, last, compare="CO2", split=False):
     fig = pplt.figure(space=0, refwidth="25em")
     axes = fig.subplots(nrows=1, ncols=2)
     axes.format(
@@ -40,13 +42,13 @@ def plot_vilion(first, last, std_type, split=False):
         xminorlocator="null",
         yminorlocator="null",
         ylocator=(first.hlayers.values.astype(int)) / 100,
-        suptitle=f"distribution of different periods with standardization of {std_type}",
+        suptitle=f"distribution of different periods",
     )
 
     modes = ["NAO", "EA"]
 
     for i, ax in enumerate(axes):
-        df = xr2df(first, last, mode=modes[i])
+        df = xr2df(first, last, mode=modes[i], compare=compare)
         g = sns.violinplot(
             data=df,
             y="hlayers",
