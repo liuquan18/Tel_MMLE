d1683591c2a0e7a97fb28f10771508b532f88c59
Quan <quan.liu@mpimet.mpg.de>
2023-01-11 16:07:17 +0100

    year

--- src/MMLE_TEL/quick_plot.py
+++ src/MMLE_TEL/quick_plot.py
@@ -260,7 +260,7 @@ class period_index:
             )
         except IndexError:
             warnings.warn("No fldmean above 4 degree. use the last 10 years instead")
-            periods.append(slice(anomaly[-11].time, anomaly[-1].time))
+            periods.append(slice(anomaly[-11].time.dt.year, anomaly[-1].time.dt.year))
         return periods
 
     def CO2_period(self):
