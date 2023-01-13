46f0dc88772f3ca8efad55815d2e8e0fa60ae03f
Quan <quan.liu@mpimet.mpg.de>
2023-01-13 18:14:32 +0100

    hlayers

---
+++ src/plots/extrc_tsurf_scatter.py
@@ -0,0 +1,39 @@
+import proplot as pplt
+import matplotlib.pyplot as plt
+
+def plot_scatter(ext_counts, tsurf, axes):
+    extr_types = ['pos','neg']  # rows
+    modes = ['NAO','EA']  # cols
+
+    for i, extr_type in enumerate(extr_types):
+        for j, mode in enumerate(modes):
+            axes[i,j].scatter(
+            x = tsurf,
+            y = ext_counts.sel(extr_type = extr_type,mode = mode)
+        )
+    return axes
+
+
+def extCount_tsurf_scatter(extc_tsuf_pairs):
+    """
+    rows: pos/neg
+    cols: NAO/EA
+    scatter: extreme_count v.s surface temperature
+    hue: different  dataset
+    """
+    fig, axes = pplt.subplots(nrows=2, ncols=2, figwidth=8, span=False, share=False)
+
+    axes.format(
+        abc="a",
+        abcloc="ul",
+        xlim=(-1, 6),
+        suptitle=f"extreme counts v.s surface temperature in decadal time scale",
+        xlabel="temperature (K)",
+        ylabel="extreme count",
+        grid=False,
+        toplabels = ['NAO','EA'],
+        leftlabels = ['pos','neg']
+    )
+
+    for ext_counts, t_surf in extc_tsuf_pairs:
+        plot_scatter(ext_counts, t_surf, axes)
