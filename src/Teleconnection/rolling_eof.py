cfea6aa21ec6d796bfc10ab361dc00f6b8046a0e
Quan <quan.liu@mpimet.mpg.de>
2023-01-13 10:14:55 +0100

    drop vars

--- src/Teleconnection/rolling_eof.py
+++ src/Teleconnection/rolling_eof.py
@@ -122,7 +122,9 @@ def changing_eofs(xarr, validtime, nmode, window):
     elif validtime.size == 1:
         tenyear_xarr = field.sel(time=validtime)
         EOFs, _, FRA = ssp.doeof(tenyear_xarr, nmode=nmode, dim="com")
-        EOFs.drop_vars("window_dim")
+        
+    # drop vars
+    EOFs = EOFs.drop_vars("window_dim")
 
     return EOFs, FRA
 
