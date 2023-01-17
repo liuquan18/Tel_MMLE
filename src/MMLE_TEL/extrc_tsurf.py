8c9c02eba729607cb7e458e57ad3434704def4cb
Quan <quan.liu@mpimet.mpg.de>
2023-01-17 18:09:16 +0100

    func and plot together

--- src/MMLE_TEL/extrc_tsurf.py
+++ src/MMLE_TEL/extrc_tsurf.py
@@ -37,3 +37,46 @@ def decadal_extrc_tsurf(index: xr.DataArray, temp: xr.DataArray, hlayers: int = 
     ext_counts = xr.concat(ext_counts, dim=periods)
     t_surf_mean = xr.concat(t_surf_mean, dim=periods)
     return ext_counts, t_surf_mean
+
+
+# plot
+
+def plot_scatter(ext_counts, tsurf, label, axes):
+    extr_types = ["pos", "neg"]  # rows
+    modes = ["NAO", "EA"]  # cols
+
+    for i, extr_type in enumerate(extr_types):
+        for j, mode in enumerate(modes):
+            axes[i, j].scatter(
+                x=tsurf,
+                y=ext_counts.sel(extr_type=extr_type, mode=mode),
+                alpha=0.5,
+                legend_kw = {'label':label}
+            )
+    return axes
+
+
+def extCount_tsurf_scatter(extc_tsuf_pairs, labels):
+    """
+    rows: pos/neg
+    cols: NAO/EA
+    scatter: extreme_count v.s surface temperature
+    hue: different  dataset
+    """
+    fig, axes = pplt.subplots(nrows=2, ncols=2, figwidth=8, span=False, share=False)
+
+    axes.format(
+        abc="a",
+        abcloc="ul",
+        xlim=(-1, 6),
+        suptitle=f"extreme counts v.s surface temperature in decadal time scale",
+        xlabel="temperature (K)",
+        ylabel="extreme count",
+        grid=False,
+        toplabels=["NAO", "EA"],
+        leftlabels=["pos", "neg"],
+    )
+
+    for i, (ext_counts, t_surf) in enumerate(extc_tsuf_pairs):
+        hs = plot_scatter(ext_counts, t_surf, label=labels[i], axes=axes)
+    axes[1,1].legend(hs, ncols=1)
