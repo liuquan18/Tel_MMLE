1d42a778de219ebb70824a0786b4d2240f6429ab
Quan <quan.liu@mpimet.mpg.de>
2023-01-17 17:58:16 +0100

    average

--- script/3mmle/Ext_count_tsurf.py
+++ script/3mmle/Ext_count_tsurf.py
@@ -5,12 +5,14 @@ import numpy as np
 
 import src.extreme.period_pattern_extreme as extreme
 import proplot as pplt
+import matplotlib.pyplot as plt
 
 import src.MMLE_TEL.extrc_tsurf as extrc_tsurf
 import src.plots.extrc_tsurf_scatter as extrc_tsurf_scatter
 
 #%%
 import importlib
+
 importlib.reload(extrc_tsurf)
 importlib.reload(extrc_tsurf_scatter)
 
@@ -18,14 +20,14 @@ importlib.reload(extrc_tsurf_scatter)
 class extr_counts_tsurf:
     def __init__(
         self,
-        models: list, # the name of the models
-        ens: list=None,
-        hlayers: int=50000, # which layer to compare
-        average: bool = None
+        models: list,  # the name of the models
+        ens: list = None,
+        hlayers: int = 50000,  # which layer to compare
+        average: bool = None,  # divided by the number of ens members
     ):
 
         # models
-        self.models = models    
+        self.models = models
         self.hlayers = hlayers
         self.average = average
         self.ens = ens
@@ -40,7 +42,7 @@ class extr_counts_tsurf:
         index = xr.open_dataset(pc_dir)
         ts = xr.open_dataset(ts_dir)
         try:
-            t_mean = ts.mean(dim = 'ens')
+            t_mean = ts.mean(dim="ens")
         except ValueError:
             t_mean = ts
 
@@ -48,13 +50,12 @@ class extr_counts_tsurf:
             t_mean = t_mean.tsurf
         except AttributeError:
             t_mean = t_mean.ts
-                
+
         index = index.pc
-        t_ano = t_mean - t_mean.isel(time = 0)
+        t_ano = t_mean - t_mean.isel(time=0)
         t_ano = t_ano.squeeze()
 
         return index, t_ano
-
 
     def extrc_tsurf_all(self):
 
@@ -63,29 +64,33 @@ class extr_counts_tsurf:
         extr_counts = []
         ts_means = []
 
-        for i,model in enumerate(self.models):
-            pc_dir = odir+model + "/EOF_result/ind_first_pc.nc"
-            ts_dir = odir+model + "/ts_processed/tsurf_mean.nc"
+        for i, model in enumerate(self.models):
+            pc_dir = odir + model + "/EOF_result/ind_first_pc.nc"
+            ts_dir = odir + model + "/ts_processed/tsurf_mean.nc"
 
             index, ts = self.read_data(pc_dir, ts_dir)
 
-            extr_count, ts_mean = extrc_tsurf.decadal_extrc_tsurf(index, ts,hlayers = self.hlayers)   
+            extr_count, ts_mean = extrc_tsurf.decadal_extrc_tsurf(
+                index, ts, hlayers=self.hlayers
+            )
             if self.average:
-                extr_count = extr_count/self.ens[i]             
+                extr_count = extr_count / self.ens[i]
             extr_counts.append(extr_count)
-            ts_means.append(ts_mean)            
-
+            ts_means.append(ts_mean)
 
         return extr_counts, ts_means
 
-
     def plot_scatter(self):
-        extrc_tsurf_scatter.extCount_tsurf_scatter(self.extr_ts_pairs,labels = self.models)
-
-
-
+        scatter = extrc_tsurf_scatter.extCount_tsurf_scatter(
+            self.extr_ts_pairs, labels=self.models
+        )
 
 
 # %%
-allm = extr_counts_tsurf(['MPI_GE','MPI_GE_onepct','CanESM2'],ens = [100,100,40],hlayers = 20000,average=True)
+allm = extr_counts_tsurf(
+    ["MPI_GE", "MPI_GE_onepct", "CanESM2"],
+    ens=[100, 100, 40],
+    hlayers=20000,
+    average=True,
+)
 # %%
