183b0b404dc9562682257911030ec0631a64712e
Quan <quan.liu@mpimet.mpg.de>
2023-02-01 11:22:25 +0100

    start confidence

---
+++ script/3mmle/confidence_interval.py
@@ -0,0 +1,13 @@
+#%% 
+# import xarray, numpy, pandas and matplotlib
+import xarray as xr
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#%%
+dir = "/work/mh0033/m300883/Tel_MMLE/data/MPI_GE_onepct/EOF_result/ind_first_pc.nc"
+ds = xr.open_dataset(dir)
+
+# %%
