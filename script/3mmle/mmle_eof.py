a43085967bc16ffea529b8951ede69572ca15d0d
Quan <quan.liu@mpimet.mpg.de>
2023-02-16 14:56:40 +0100

    nothing

--- script/3mmle/mmle_eof.py
+++ script/3mmle/mmle_eof.py
@@ -8,7 +8,7 @@ import src.MMLE_TEL.quick_plot as quick_plot
 
 # config
 v_eof = 'ind' # vertical_eof
-fpattern = 'first' # fixed pattern
+fpattern = 'all' # fixed pattern
 
 # %% 
 # generate index
@@ -26,9 +26,6 @@ mpige.save_result()
 #%%
 ## MPI-GE_onepct
 mpige_onepct = index_generate.decompose_fixedPattern("MPI_GE_onepct",v_eof,fpattern)
-
-#%%
-mpige_onepct.save_result()
 
 
 #%%
