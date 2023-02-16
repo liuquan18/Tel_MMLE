2b4dd07100b4fe7ff8ee0089fe5f30186fd51208
Quan <quan.liu@mpimet.mpg.de>
2023-02-16 15:11:21 +0100

    modular

--- src/MMLE_TEL/index_generator.py
+++ src/MMLE_TEL/index_generator.py
@@ -21,31 +21,68 @@ class decompose_fixedPattern:
         self.fixed_pattern = fixed_pattern
         self.standard_ens_time = standard_ens_time
         self.model = model
-
+        self.odir = "/work/mh0033/m300883/Tel_MMLE/data/" + self.model + "/"
         self.zg_path = (
-            "/work/mh0033/m300883/Tel_MMLE/data/" + self.model + "/zg_processed/"
+            self.odir + "zg_processed/"
+        )
+        self.save_path = (
+            self.odir + "EOF_result/"
         )
 
         # read data
         print("reading the gph data ...")
         self.data = season_eof.read_data(self.zg_path)
+        self.eof_result = self.decompose()
 
+    def decompose(self):
         # deompose
         print("decomposing the data ...")
-        self.eof_result = season_eof.season_eof(
+        eof_result = season_eof.season_eof(
             self.data,
             nmode=2,
             window=10,
             fixed_pattern=self.fixed_pattern,
             independent=self.independence,
-            standard=False, # standardization is done seperately
+            standard=False,  # standardization is done seperately
         )
+        return eof_result
 
-        # save the result
-        self.save_path = (
-            "/work/mh0033/m300883/Tel_MMLE/data/" + self.model + "/EOF_result/"
-        )
+
+    def standard_index(self):
+        print("standardizing the index ...")
+        # if single pattern, standardize the index with its own mean and std (temporal and ens)
+        if (
+            self.fixed_pattern == "first"
+            or self.fixed_pattern == "last"
+            or self.fixed_pattern == "all"
+        ):
+            self.eof_result["pc"] = (
+                self.eof_result["pc"] - self.eof_result["pc"].mean(dim=("time", "ens"))
+            ) / self.eof_result["pc"].std(dim=("time", "ens"))
+
+        # if changing pattern, standardize the index with the mean and std of all the index
+        elif self.fixed_pattern == "decade" or self.fixed_pattern == "False":
+            try:
+                all_index = xr.open_dataset(
+                self.odir + "EOF_result/" + self.vertical_eof + "_all_eof_result.nc"
+                ).pc
+
+            except FileNotFoundError:
+                print("all index not found, generate it first")
+            
+            self.eof_result["pc"] = (
+                self.eof_result["pc"] - all_index["pc"].mean(dim=("time", "ens"))
+            ) / all_index["pc"].std(dim=("time", "ens"))
+
+        else:
+            print("wrong fixed pattern")
+        return self.eof_result
+
+    # save
+    def save(self):
         print("saving the result ...")
+        # save the result
+
         self.eof_result.to_netcdf(
             self.save_path
             + self.vertical_eof
@@ -55,6 +92,5 @@ class decompose_fixedPattern:
             + "eof_result.nc"
         )
 
- 
 
 # %%
