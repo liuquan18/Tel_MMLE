445251ca91902b47a2a786d75fe5cb6381a7496b
Quan <quan.liu@mpimet.mpg.de>
2023-02-20 16:46:34 +0100

    dep decade

--- script/3mmle/story_line.py
+++ script/3mmle/story_line.py
@@ -7,8 +7,11 @@ import importlib
 importlib.reload(story_line)
 # %%
 mpige_onepct_ind_decade = story_line.story_line("MPI_GE_onepct", "ind", "decade")
-# %%
 mpige_onepct_ind_decade.plot_all()
-# %%
 mpige_onepct_ind_decade.create_doc()
 # %%
+mpige_onepct_dep_decade = story_line.story_line("MPI_GE_onepct", "dep", "decade")
+mpige_onepct_dep_decade.plot_all()
+mpige_onepct_dep_decade.create_doc()
+# %%
+mpige_onepct_dep_decade.stat_overview()
