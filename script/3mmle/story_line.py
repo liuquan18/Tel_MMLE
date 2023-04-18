dd688eba91e18110f1210e40faa24cce41d045ff
Quan <quan.liu@mpimet.mpg.de>
2023-04-18 11:41:49 +0200

    debug name

--- script/3mmle/story_line.py
+++ script/3mmle/story_line.py
@@ -6,7 +6,7 @@ import src.MMLE_TEL.story_line as story_line
 import importlib
 importlib.reload(story_line)
 # %%
-mpige_onepct_ind_decade = story_line.story_line("CESM1_CAM5", "ind", "decade")
+mpige_onepct_ind_decade = story_line.story_line("MPI_GE_onepct", "ind", "decade")
 
 #%%
 mpige_onepct_ind_decade.extreme_count_profile(xlim = (20,120))
