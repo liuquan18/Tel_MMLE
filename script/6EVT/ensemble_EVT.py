dc0fbbeec75bf4fba6d852a9dcaf0de66a5aeb6a
Quan <quan.liu@mpimet.mpg.de>
2023-04-19 14:17:21 +0200

    for slides plot

--- script/6EVT/ensemble_EVT.py
+++ script/6EVT/ensemble_EVT.py
@@ -4,6 +4,7 @@ import numpy as np
 import xarray as xr
 import pandas as pd
 from pyextremes import EVA
+from datetime import date
 # %%
 # load data
 eof = xr.open_dataset('/work/mh0033/m300883/Tel_MMLE/data/MPI_GE_onepct/EOF_result/ind_first_eof_result.nc')
@@ -12,7 +13,6 @@ pc = eof.pc
 # %%
 NAO500 = pc.sel(plev = 50000, mode = 'NAO')
 # %%
-# %%
 ex = NAO500.copy()
 ex = ex.isel(time = slice(0,10))
 ex = ex.stack(com = ('time','ens'))
@@ -20,8 +20,13 @@ ex = ex.stack(com = ('time','ens'))
 ex = ex.drop_vars(['time','ens']).squeeze()
 # %%
 # a new dim called time, which is a 1000-year-long time object
-years = np.arange(2000,3001)
-years = years.astype(str)
-time = pd.to_datetime(years)
+years = np.arange(2000,3000)
+years = [date(year,1,1) for year in years]
 # %%
 # generate a pd.date_range object, 1000 years long, with freq = 'Y'
+df = pd.Series(ex.values, index = years)
+# %%
+model = EVA(df)
+# %%
+df.index
+# %%
