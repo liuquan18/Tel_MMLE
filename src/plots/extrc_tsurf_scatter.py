cb4fe06809d483cd89616d6205aa22171c545d78
Quan <quan.liu@mpimet.mpg.de>
2023-05-23 10:40:38 +0200

    single plev

--- src/plots/extrc_tsurf_scatter.py
+++ src/plots/extrc_tsurf_scatter.py
@@ -1,22 +1,13 @@
+#%%
+import xarray as xr
+import numpy as np
+import src.extreme.extreme_ci as extreme
 import proplot as pplt
-import matplotlib.pyplot as plt
 
-
-def plot_scatter(ext_counts, tsurf, axes):
-    extr_types = ["pos", "neg"]  # rows
-    modes = ["NAO", "EA"]  # cols
-
-    for i, extr_type in enumerate(extr_types):
-        for j, mode in enumerate(modes):
-            axes[i, j].scatter(
-                x=tsurf,
-                y=ext_counts.sel(extr_type=extr_type, mode=mode),
-                alpha=0.5,
-            )
-    return axes
-
-
-def extCount_tsurf_scatter(extc_tsuf_pairs, labels):
+#%%
+def extCount_tsurf_scatter(
+    ext_counts, t_surf, ylim=(0, 55), xlim=(-1, 5), xlabel="temperature (K)"
+):
     """
     rows: pos/neg
     cols: NAO/EA
@@ -28,15 +19,80 @@ def extCount_tsurf_scatter(extc_tsuf_pairs, labels):
     axes.format(
         abc="a",
         abcloc="ul",
-        xlim=(-1, 6),
+        xlim=xlim,
         suptitle=f"extreme counts v.s surface temperature in decadal time scale",
-        xlabel="temperature (K)",
+        xlabel=xlabel,
         ylabel="extreme count",
         grid=False,
-        toplabels=["NAO", "EA"],
-        leftlabels=["pos", "neg"],
+        leftlabels=["NAO", "EA"],
+        toplabels=["pos", "neg"],
+        xminorticks="null",
+        yminorticks="null",
+        ylim=ylim,
     )
 
-    for i, (ext_counts, t_surf) in enumerate(extc_tsuf_pairs):
-        hs = plot_scatter(ext_counts, t_surf, label=labels[i], axes=axes)
-    axes[1,1].legend(hs, ncols=1)
+    scatter_plot(ext_counts, t_surf,  axes)
+
+
+def scatter_plot(ext_counts, t_surf,  axes):
+    for j, extr_type in enumerate(ext_counts.extr_type):
+        for i, mode in enumerate(ext_counts.mode):
+            # data preparation
+            true = ext_counts.sel(extr_type=extr_type, mode=mode, confidence="true")
+            low = ext_counts.sel(extr_type=extr_type, mode=mode, confidence="low")
+            high = ext_counts.sel(extr_type=extr_type, mode=mode, confidence="high")
+
+            # for the data with plev
+            try:
+                true = true.stack(com=("time", "plev"))
+                low = low.stack(com=("time", "plev"))
+                high = high.stack(com=("time", "plev"))
+                t = t_surf.stack(com=("time", "plev"))
+
+            except KeyError:
+                t = t_surf
+
+            axes[i, j].errorbar(
+                x=t,
+                y=true,
+                yerr=[(true - low), (high - true)],
+                fmt="o",
+                linewidth=2,
+                capsize=6,
+            )
+
+# %%
+def decadal_extrc_tsurf(index: xr.DataArray, temp: xr.DataArray, plev=None):
+    """
+    extract the extreme count and the mean surface temperature every ten years.
+    **Arguments**
+        *index* the DataArray of pc index
+        *temp* the -fldmean, -yearmean -ensmean surface temperauter.
+    **Return**
+        *ext_counts* the DataArray of extreme count, *time, *extr_type, *mode
+        *t_surf_mean* the mean t_surface (the increase of the temperature)
+        the time here use the first year of the decade.
+    """
+    if plev is not None:
+        index = index.sel(plev=plev)
+
+    ext_counts = []
+    t_surf_mean = []
+    for i in range(0, index.time.size, 10):
+
+        period = slice(i, i + 10)
+
+        period_pc = index.isel(time=period)
+        period_tm = temp.isel(time=period)
+
+        # extreme count
+        period_ext_count = extreme.extreme_count_xr(period_pc)
+        period_mean_t = period_tm.mean(dim="time")
+
+        ext_counts.append(period_ext_count)
+        t_surf_mean.append(period_mean_t)
+
+    periods = index.isel(time=np.arange(0, index.time.size, 10)).time
+    ext_counts = xr.concat(ext_counts, dim=periods)
+    t_surf_mean = xr.concat(t_surf_mean, dim=periods)
+    return ext_counts, t_surf_mean
