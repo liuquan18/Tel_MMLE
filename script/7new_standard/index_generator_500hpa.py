74b72a310cd3c5de2ecd6af3714a86f61eb06aa8
Quan <quan.liu@mpimet.mpg.de>
2023-05-26 11:50:16 +0200

    generated

--- script/7new_standard/index_generator_500hpa.py
+++ script/7new_standard/index_generator_500hpa.py
@@ -1,9 +1,5 @@
 #%%
 #%%
-import xarray as xr
-import numpy as np
-import pandas as pd
-
 import src.MMLE_TEL.index_generator as index_generate
 # %%
 MPIGE_decade = index_generate.decompose_plev("MPI_GE_onepct",plev = 50000,fixedPattern = 'decade',standard='temporal_ens')
@@ -24,4 +20,10 @@ CESM_all.save_result()
 Can_all = index_generate.decompose_plev("CanESM2",plev = 50000,fixedPattern = 'all',standard='temporal_ens')
 # %%
 Can_all.save_result()
+# %%
+
+# %%
+MPIGE_decade_trop = index_generate.decompose_troposphere("MPI_GE_onepct",vertical_eof='ind',fixedPattern = 'decade',standard='temporal_ens')
+# %%
+MPIGE_decade_trop.save_result()
 # %%
