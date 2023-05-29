92e340a525b335b730bb8b0ee5e267bbe3e92088
Quan <quan.liu@mpimet.mpg.de>
2023-05-29 19:46:28 +0200

    generated

--- script/7new_standard/index_generator_500hpa.py
+++ script/7new_standard/index_generator_500hpa.py
@@ -2,6 +2,8 @@
 #%%
 import src.MMLE_TEL.index_generator as index_generate
 import xarray as xr
+import numpy as np
+import cartopy.crs as ccrs
 # %%
 def standard(generator):
     pc = generator.eof_result['pc']
@@ -27,6 +29,12 @@ index_gen('CESM1_CAM5','all')
 # MK3.6
 index_gen('MK36','decade')
 index_gen('MK36','all')
+#%%
+# GFDL_CM3
+index_gen('GFDL_CM3','decade')
+index_gen('GFDL_CM3','all')
+
+
 # %%
 def read_eof(model,fixedPattern):
     eof_dir = f"/work/mh0033/m300883/Tel_MMLE/data/{model}/EOF_result/plev_50000_{fixedPattern}_temporal_ens_eof_result.nc"
@@ -43,4 +51,25 @@ CESM_all_eof = read_eof('CESM1_CAM5','all')
 # MK3.6
 MK36_decade_eof = read_eof('MK36','decade')
 MK36_all_eof = read_eof('MK36','all')
+# %%
+# GFDL_CM3
+GFDL_decade_eof = read_eof('GFDL_CM3','decade')
+GFDL_all_eof = read_eof('GFDL_CM3','all')
+# %%
+# plot the GFDL_CM3_decade EOF with the projection of projection=ccrs.Orthographic(-20,60)
+def quick_plot(GFDL_decade_eof):
+    GFDL_decade_eof.eof.sel(decade="1920-12-31",method = 'nearest').squeeze().plot.contourf(col = 'mode',
+    subplot_kws=dict(projection=ccrs.Orthographic(-20,60)),
+    levels=np.arange(-1.5,1.6,0.3),
+    transform=ccrs.PlateCarree(),
+)
+
+#%%
+quick_plot(GFDL_decade_eof)
+# %%
+quick_plot(Can_decade_eof)
+# %%
+quick_plot(CESM_decade_eof)
+# %%
+quick_plot(MK36_decade_eof)
 # %%
