ef737470ea43c418352ecbfd76f8e3bd7d635801
Quan <quan.liu@mpimet.mpg.de>
2023-06-09 17:43:34 +0200

    tools

---
+++ script/7new_standard/tmp_ens_mean_exist.py
@@ -0,0 +1,14 @@
+#%%
+import os
+from pathlib import Path
+# %%
+odir = Path("/work/mh0033/m300883/Tel_MMLE/data/")
+models = ["MPI_GE_onepct", "MPI_GE", "CanESM2", "CESM1_CAM5", "GFDL_CM3", "MK36"]
+for model in models:
+    ts_p_dir = odir / model / "ts_processed" / "ens_mean.nc"
+    # check if the file exists
+    if os.path.exists(ts_p_dir):
+        continue
+    else:
+        print(f"{model} doesn't exists")
+# %%
