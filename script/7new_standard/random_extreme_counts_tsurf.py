88f205e21fac3d2ccce66fb4f3ef248eaac1a8c8
Quan <quan.liu@mpimet.mpg.de>
2023-06-20 16:22:43 +0200

    generated

--- script/7new_standard/random_extreme_counts_tsurf.py
+++ script/7new_standard/random_extreme_counts_tsurf.py
@@ -13,44 +13,57 @@ importlib.reload(index_stats)
 
 
 # %%
-def read_data( ens_size,model = 'MPI_GE_onepct',plev = 50000,standard = 'first'):
+def read_data( ens_size,model = 'MPI_GE_onepct',plev = 50000,standard = 'first',tsurf = 'ens_fld_year_mean'):
     eof_dir = f"/work/mh0033/m300883/Tel_MMLE/data/{model}_random/EOF_result/plev_{plev}_decade{str(ens_size)}_{standard}_eof_result.nc"
     eof_result = xr.open_dataset(eof_dir)
 
-    tsurf_dir = f"/work/mh0033/m300883/Tel_MMLE/data/{model}_random/ts_processed/ens_fld_year_mean.nc"
+    tsurf_dir = f"/work/mh0033/m300883/Tel_MMLE/data/{model}_random/ts_processed/{tsurf}.nc"
     tsurf = xr.open_dataset(tsurf_dir).tsurf
-    tsurf = tsurf.mean(dim = 'ens').squeeze()
 
-    return eof_result, tsurf
+    extrc_dir = f"/work/mh0033/m300883/Tel_MMLE/data/{model}_random/extreme_count/plev_{plev}_decade{str(ens_size)}_{standard}_extre_counts.nc"
+    try:
+        extrc = xr.open_dataset(extrc_dir).pc
+    except FileNotFoundError:
+        extrc = None
+    return eof_result, tsurf, extrc
 
 # %%
 # count the extreme event and tsurf
-def extreme_counts (eof_result, tsurf):
-    tsurf_increase = tsurf - tsurf[0]
-    ext_counts, t_surf_mean = extrc_tsurf.decadal_extrc_tsurf(
-        eof_result.pc, tsurf_increase,ci = 'bootstrap'
-    )
+def extreme_counts (eof_result, tsurf, extrc = None):
 
-    ds = xr.Dataset(
-        {
-            "extreme_counts": ext_counts,
-            "tsurf": t_surf_mean,
-        }
-    )
-    return ds
+    tsurf_increase = tsurf - tsurf[0]
+    if extrc is None:
+        ext_counts, t_surf_mean = extrc_tsurf.decadal_extrc_tsurf(
+            eof_result.pc, temp = tsurf_increase,ci = 'bootstrap'
+        )
+    else:
+        ext_counts, t_surf_mean = extrc_tsurf.decadal_extrc_tsurf(
+            eof_result.pc, ext_counts_xr = extrc, temp=tsurf_increase, ci = 'bootstrap'
+        )
+    return ext_counts, t_surf_mean
 
 # %%
 from multiprocessing import Pool
 
 ens_sizes = np.arange(20,101,10)
+tsurfs = ["ens_fld_year_mean","NA_tsurf","tropical_arctic_gradient"]
 
-def process_ens_size(ens_size,standard = 'first'):
-    plev = 50000
-    eof_result, tsurf = read_data(ens_size,plev = plev)
-    ds = extreme_counts(eof_result, tsurf)
-    ds.to_netcdf(f"/work/mh0033/m300883/Tel_MMLE/data/MPI_GE_onepct_random/extreme_count/plev_{plev}_{standard}_extreme_counts_tsurf_{str(ens_size)}.nc")
+def process_data(ens_size, tsurf):
+    eof_result, temperature, extrc= read_data(ens_size,tsurf = tsurf)
+    # close the files red
+    temperature.close()
+    
+    extr_counts, tsurf_mean = extreme_counts(eof_result, temperature, extrc=extrc)
+    try:
+        extr_counts.to_netcdf(f"/work/mh0033/m300883/Tel_MMLE/data/MPI_GE_onepct_random/extreme_count/plev_50000_first_extreme_counts_{str(ens_size)}_{tsurf}.nc")
+    except PermissionError:
+        pass
+    tsurf_mean.to_netcdf(f"/work/mh0033/m300883/Tel_MMLE/data/MPI_GE_onepct_random/extreme_count/{tsurf}.nc")
 
-with Pool() as p:
-    p.map(process_ens_size, ens_sizes)
+if __name__ == '__main__':
+    with Pool() as p:
+        p.starmap(process_data, [(ens_size, tsurf) for ens_size in ens_sizes for tsurf in tsurfs])
+    
+
 
 # %%
