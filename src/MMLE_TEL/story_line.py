ddc45fe8662437c6034510a1c8e9a511ef0b4b42
Quan <quan.liu@mpimet.mpg.de>
2023-06-26 13:53:08 +0200

    story_line new file

--- src/MMLE_TEL/story_line.py
+++ src/MMLE_TEL/story_line.py
@@ -1,6 +1,5 @@
 # %%
 # import xarray, numpy, pandas, proplot
-import numpy as np
 import pandas as pd
 import proplot as pplt
 import matplotlib.pyplot as plt
@@ -8,10 +7,11 @@ import matplotlib.patches as mpatches
 from matplotlib.lines import Line2D
 import seaborn as sns
 import xarray as xr
+import numpy as np
 
 # extremes
 import src.extreme.extreme_ci as extreme
-import src.MMLE_TEL.extrc_tsurf as extrc_tsurf
+import src.plots.extrc_tsurf_scatter as extrc_tsurf
 import src.MMLE_TEL.spatial_pattern_change as sp_change
 
 # reimport extreme
@@ -25,6 +25,8 @@ import src.plots.statistical_overview as stat_overview
 
 # composite analysis
 import src.composite.field_composite as composite
+import src.plots.NAO_EA_hist2d as hist2d
+
 
 #%%
 importlib.reload(extreme)
@@ -34,210 +36,235 @@ importlib.reload(warming_stage)
 importlib.reload(stat_overview)
 
 #%%
-# class story line
+# class index_stats
 class story_line:
-    """
-    generating the plots for the paper
-    """
+    """The class to calculate and plot the statistics of the indices"""
 
-    def __init__(self, model, vertical_eof, fixed_pattern):
+    def __init__(
+        self,
+        model,
+        vertical_eof,
+        fixed_pattern,
+        standard="temporal_ens",
+        tsurf="ens_fld_year_mean",
+        plev=50000,
+        season="DJFM",
+    ) -> None:
         self.model = model
         self.vertical_eof = vertical_eof
         self.fixed_pattern = fixed_pattern
-        self.prefix = self.vertical_eof + "_" + self.fixed_pattern + "_"
+        self.standard = standard
+        self.plev = plev
+        self.season = season
+        self.tsurf = tsurf
 
-        # locations
-        odir = "/work/mh0033/m300883/Tel_MMLE/data/" + self.model + "/"
-        self.eof_result_dir = odir + "EOF_result/" + self.prefix + "own_eof_result.nc"
-        self.tsurf_dir = odir + "ts_processed/tsurf_mean.nc"
-        self.ts_fullfield_dir = (
-            odir + "ts/"
-        )  # the location for the full field temperature data
-        self.atg_dir = odir + "ts_processed/tsurf_anom_gradient.nc"
 
-        ## locations to save
+        self.prefix = (
+            f"plev_{self.plev}_"
+            + self.fixed_pattern
+            + "_"
+            + self.standard
+            + "_"
+            + self.season
+        )
+
+        # locations to read
+        self.odir = "/work/mh0033/m300883/Tel_MMLE/data/" + self.model + "/"
+        self.eof_result_dir = self.odir + f"EOF_result/{self.prefix}_eof_result.nc"
+        self.extre_counts_500hpa_dir = (
+            self.odir + f"extreme_count/{self.prefix}_extre_counts.nc"
+        )
+        self.extre_counts_trop_first_dir = (
+            self.odir
+            + f"extreme_count/troposphere_ind_decade_{self.standard}_{self.season}_first_count.nc"
+        )
+        self.extre_counts_trop_last_dir = (
+            self.odir
+            + f"extreme_count/troposphere_ind_decade_{self.standard}_{self.season}_last_count.nc"
+        )
+        self.tsurf_dir = self.odir + f"ts_processed/{self.tsurf}.nc"
+        self.field_tsurf_dir = self.odir + f"ts/{self.tsurf}.nc"
+
+        # locations to save
         self.to_plot_dir = (
-            "/work/mh0033/m300883/Tel_MMLE/docs/source/plots/new_standard/" + self.prefix
+            "/work/mh0033/m300883/Tel_MMLE/docs/source/plots/Winter_Summer/"
+            + self.model
+            + "_"
+            + self.prefix
         )
         self.doc_dir = "/work/mh0033/m300883/Tel_MMLE/docs/source/"
 
         # read eof
-        self.eof_result = self.read_eof()
-        self.eof = self.eof_result.eof
-        self.fra = self.eof_result.fra
-        self.pc = self.eof_result.pc
+        self.eof_result = xr.open_dataset(self.eof_result_dir)
+        self.extre_counts_500hpa = xr.open_dataset(self.extre_counts_500hpa_dir).pc
+        self.extre_counts_trop_first = xr.open_dataset(self.extre_counts_trop_first_dir).pc
+        self.extre_counts_trop_last = xr.open_dataset(self.extre_counts_trop_last_dir).pc
+        self.tsurf = self.read_tsurf()
 
-        # read tsurf
-        print("reading tsurf data...")
-        self.tsurf = warming_stage.read_tsurf_fldmean(self.tsurf_dir)
-        print("reading tsurf gradient data...")
-        self.atg = warming_stage.read_tsurf_fldmean(self.atg_dir)
-        self.warming_periods = warming_stage.temp_period(self.tsurf)
+    #%%
+    # read tsurf
+    def read_tsurf(self):
+        tsurf = xr.open_dataset(self.tsurf_dir)
 
-        # split index into first 10 and last 10 years
-        periods_pc, self.periods = warming_stage.split_period(self.pc, compare="CO2")
-        self.first_pc, self.last_pc = periods_pc[0], periods_pc[1]
-        self.first_eof = self.eof.sel(decade=self.periods[0])  # not time
-        self.last_eof = self.eof.sel(decade=self.periods[-1])
+        # read the array either tusrf, ts, or tas
+        try:
+            tsurf_arr = tsurf.tsurf.squeeze()
+        except AttributeError:
+            try:
+                tsurf_arr = tsurf.ts.squeeze()
+            except AttributeError:
+                tsurf_arr = tsurf.tas.squeeze()
+        # change the temp time into datetime64
+        try:
+            tsurf_arr["time"] = tsurf_arr.indexes["time"].to_datetimeindex()
+        except AttributeError:
+            pass
+        return tsurf_arr
 
-        # extreme event count
-        self.first_count = extreme.extreme_count_xr(self.first_pc)
-        self.last_count = extreme.extreme_count_xr(self.last_pc)
-
-    # read eof data
-    def read_eof(self):
-        print("reading eof result data...")
-        eof_result = xr.open_dataset(self.eof_result_dir)
-        return eof_result
-
-    # read temperature data to do the composite analysis
-    def read_var(self):
-        """
-        read the tsurf or wind, precipitation for composite analysis
-        data are stored in 3rdPanel/data/
-        """
-        print("reading the var to do composite...")
-        # MPI is different format...
-        if self.model == "MPI_GE_onepct":
-            var_data = xr.open_dataset(self.ts_fullfield_dir + "all_ens_tsurf.nc").tsurf
-        elif self.model == "MPI_GE":
-            var_data = xr.open_mfdataset(
-                self.ts_fullfield_dir + "*.nc", combine="nested", concat_dim="ens"
-            ).tsurf
-        else:
-            var_data = xr.open_mfdataset(
-                self.ts_fullfield_dir + "*.nc", combine="nested", concat_dim="ens"
-            )
-            var_data = var_data.ts
-            var_data = var_data.rename({"plev": "plev"})
-            var_data["time"] = var_data.indexes["time"].to_datetimeindex()
-        var_data = var_data - var_data.mean(dim="ens")  # remove the ensemble mean
-
-        return var_data
-
-    # statistical overview
-    def stat_overview(self):
+    #%%
+    # stat overview
+    def stat_overview(self, levels=np.arange(-2, 2.1, 0.4)):
         print("ploting the statistical overview")
-        Fig1 = stat_overview.stat_overview(self.eof_result)
-        plt.savefig(self.to_plot_dir + "stat_overview.png")
+        first_pc = self.eof_result.pc.isel(time=slice(0, 10))
+        last_pc = self.eof_result.pc.isel(time=slice(-10, None))
+        first_eof = None
+        last_eof = None
+        first_fra = None
+        last_fra = None
 
-    # spatial pattern change at 0K, 2K, 4K
-    def spatial_pattern_change(self, plev=50000, **kwargs):
-        print("ploting the spatial pattern change at 0K, 2K, 4K")
-        Fig2 = sp_change.spatial_pattern_change_decade(
-            self.warming_periods, self.eof, self.fra, **kwargs
-        )
-        plt.savefig(
-            self.to_plot_dir
-            + "spatial_pattern_change"
-            + f"_{(plev/100):.0f}hPa"
-            + ".png"
-        )
+        if self.fixed_pattern == "decade":
+            first_eof = self.eof_result.eof.isel(decade=0)
+            last_eof = self.eof_result.eof.isel(decade=-1)
 
-    # extreme event count profile
+            first_fra = self.eof_result.fra.isel(decade=0)
+            last_fra = self.eof_result.fra.isel(decade=-1)
+        elif self.fixed_pattern == "all":
+            first_eof = self.eof_result.eof
+            last_eof = None
+
+            first_fra = self.eof_result.fra
+            last_fra = None
+
+        stat_overview_fig = stat_overview.stat_overview(
+            first_pc, last_pc, first_fra, last_fra, first_eof, last_eof, levels=levels
+        )
+        plt.savefig(self.to_plot_dir + "_stat_overview.png", dpi=300)
+
+    # 2d hist of NAO and EA in the first10 and last10 decades
+    def NAO_EA_hist2d(self, bins=20, levels=np.arange(0, 0.31, 0.03)):
+        print("ploting the 2d hist of NAO and EA in the first10 and last10 decades")
+        hist2d.NAO_EA_hist2d(self.eof_result.pc, bins=bins, levels=levels)
+        plt.savefig(self.to_plot_dir + "_NAO_EA_hist2d.png", dpi=300)
+
+    # plot the vertical profile of the extreme counts
     def extreme_count_profile(self, **kwargs):
+        print("reading the eof result with whole troposphere")
+        # extreme counts of first and last 10 decades
+
         print("ploting the extreme event count profile")
         extreme_profile = extreme.extreme_count_profile(
-            self.first_count, self.last_count, colored=False, **kwargs
+            self.extre_counts_trop_first, self.extre_counts_trop_last, colored=False, **kwargs
         )
-        plt.savefig(self.to_plot_dir + "extreme_count_vertical_profile.png")
+        plt.savefig(
+            self.to_plot_dir[:-44]
+            + f"{self.model}_extreme_count_vertical_profile.png"
+        )
+        # slightly different path for the fig.
 
     # extreme event count vs. tsurf
-    def extrc_tsurf(self, plev=50000, ylim=(5, 65)):
-        print("ploting the extreme event count vs. tsurf")
-        tsurf_mean = self.tsurf
-        tsurf_increase = tsurf_mean - tsurf_mean[0]
-        ext_counts, t_surf_mean = extrc_tsurf.decadal_extrc_tsurf(
-            self.pc, tsurf_increase
-        )
-        Fig3 = extrc_tsurf.extCount_tsurf_scatter(
-            ext_counts, t_surf_mean, plev=plev, ylim=ylim
-        )
-        plt.savefig(
-            self.to_plot_dir + "extreme_count_tsurf" + f"_{(plev/100):.0f}hPa" + ".png"
-        )
+    def extrc_tsurf(self, ylim=(35, 110)):
 
-    # extreme event count vs. arctic-tropical gradient
-    def extrc_atg(self, plev=50000, ylim=(-5, 65)):
-        print("ploting the extreme event count vs. arctic-tropical gradient")
-        atg = self.atg
-        ext_counts, atg_dec = extrc_tsurf.decadal_extrc_tsurf(self.pc, atg)
-        extc_atg_scatter = extrc_tsurf.extCount_tsurf_scatter(
-            ext_counts,
-            atg_dec,
-            plev=plev,
-            ylim=ylim,
-            xlim=(-7, 7),
-            xlabel="tropic-arctic gradient",
+        extrc_tsurf_scatter = extrc_tsurf.extCount_tsurf_scatter(
+            self.extre_counts_500hpa, self.tsurf, ylim=ylim
         )
-        plt.savefig(
-            self.to_plot_dir + "extreme_count_atg" + f"_{(plev/100):.0f}hPa" + ".png"
-        )
+        plt.savefig(self.to_plot_dir + f"_extreme_count_tsurf.png", dpi=300)
 
     # composite analysis of surface temperature in terms of different extreme events
-    def composite_analysis(self, plev=50000, **kwargs):
+    def composite_analysis(self, reduction="mean", **kwargs):
         print("ploting the composite analysis of surface temperature")
-        var_data = self.read_var()
+        print(" reading the tsurf data...")
+        var_data = xr.open_dataset(self.field_tsurf_dir + "all_ens_tsurf.nc").tsurf
+        var_data = var_data - var_data.mean(dim="ens")
 
-        first_index = self.first_pc.sel(plev=plev)
-        last_index = self.last_pc.sel(plev=plev)
+        first_index = self.eof_result.pc.isel(time=slice(0, 10))
+        last_index = self.eof_result.pc.isel(time=slice(-10, None))
 
-        first_var = composite.Tel_field_composite(first_index, var_data,reduction='mean',threshold=1.5)
-        last_var = composite.Tel_field_composite(last_index, var_data,reduction='mean',threshold=1.5)
+        print(" compositing the tsurf data...")
+        first_var = composite.Tel_field_composite(
+            first_index, var_data, threshold=1.5, reduction=reduction, **kwargs
+        )
+        last_var = composite.Tel_field_composite(
+            last_index, var_data, threshold=1.5, reduction=reduction, **kwargs
+        )
 
         temp_NAO = composite.composite_plot(first_var, last_var, "NAO")
         plt.savefig(
-            self.to_plot_dir + f"composite_tsurf_NAO.png",
+            self.to_plot_dir + f"_composite_tsurf_NAO.png",
             dpi=300,
         )
 
         temp_EA = composite.composite_plot(first_var, last_var, "EA")
         plt.savefig(
-            self.to_plot_dir  + f"composite_tsurf_EA.png",
+            self.to_plot_dir + f"_composite_tsurf_EA.png",
             dpi=300,
         )
 
+    # plot all
     def plot_all(self):
         self.stat_overview()
-        self.spatial_pattern_change()
-        self.extreme_count_profile(xlim=(20, 120))
-        self.extrc_tsurf(ylim=(25, 130))
-        self.extrc_atg(ylim=(25, 130))
-        self.composite_analysis()
+        self.NAO_EA_hist2d()
+        self.extreme_count_profile()
+        self.extrc_tsurf()
 
-    def create_doc(self):
-        """create md file for the plots"""
-        print("creating md files")
-        with open(self.doc_dir + self.prefix + "story_line.md", "w") as f:
-            f.write(f"# {self.prefix}Story line\n")
-            f.write("## Statistical overview\n")
+    # write the above plots into a markdown file
+    def write_doc(self):
+        """create the md file for the plots"""
+        relative_plot_dir = (
+            "plots/new_standard/"
+            + self.model
+            + "_plev_50000_"
+            + self.fixed_pattern
+            + "_"
+            + self.standard
+            + "_"
+        )
+        print("creating the markdown file for the plots")
+        with open(
+            self.doc_dir + self.model + "_" + self.fixed_pattern + "_index_stats.md",
+            "w",
+        ) as f:
+            f.write("# Statistics of the indices\n")
+
+            f.write("This file contains the statistics of the indices\n")
+
+            f.write("## statistical overview\n")
             f.write(
-                f"![stat_overview](plots/story_line/{self.prefix}stat_overview.png)\n"
+                "The first EOF and PC of the first 10 decades and the last 10 decades are shown below\n"
             )
-            f.write("## Spatial pattern change at 0K, 2K, 4K\n")
+            f.write(f"![statistical overview]({relative_plot_dir}stat_overview.png)\n")
+
+            f.write("## 2d hist of NAO and EA in the first10 and last10 decades\n")
             f.write(
-                f"![spatial_pattern_change](plots/story_line/{self.prefix}spatial_pattern_change_500hPa.png)\n"
+                f"![2d hist of NAO and EA in the first10 and last10 decades]({relative_plot_dir}NAO_EA_hist2d.png)\n"
             )
-            f.write("## Extreme event count profile\n")
+
+            f.write("## extreme event count profile\n")
             f.write(
-                f"![extreme_count_vertical_profile](plots/story_line/{self.prefix}extreme_count_vertical_profile.png)\n"
+                f"![extreme event count profile](plots/new_standard/{self.model}_extreme_count_vertical_profile.png)\n"
             )
-            f.write("## Extreme event count vs. tsurf\n")
+
+            f.write("## extreme event count  vs. tsurf\n")
             f.write(
-                f"![extreme_count_tsurf](plots/story_line/{self.prefix}extreme_count_tsurf_500hPa.png)\n"
+                f"![extreme event count vs. tsurf]({relative_plot_dir}_extreme_count_tsurf.png)\n"
             )
-            f.write("## Extreme event count vs. arctic-tropical gradient\n")
+
+
             f.write(
-                f"![extreme_count_atg](plots/story_line/{self.prefix}extreme_count_atg_500hPa.png)\n"
+                "## composite analysis of surface temperature in terms of different extreme events\n"
             )
             f.write(
-                "## Composite analysis of surface temperature in terms of different extreme events\n"
+                f"![composite analysis of surface temperature in terms of different extreme events]({relative_plot_dir}composite_tsurf_NAO.png)\n"
             )
-            f.write("### NAO\n")
             f.write(
-                f"![composite_tsurf_NAO](plots/story_line/{self.prefix}composite_tsurf_NAO.png)\n"
-            )
-            f.write("### EA\n")
-            f.write(
-                f"![composite_tsurf_EA](plots/story_line/{self.prefix}composite_tsurf_EA.png)\n"
+                f"![composite analysis of surface temperature in terms of different extreme events]({relative_plot_dir}composite_tsurf_EA.png)\n"
             )
