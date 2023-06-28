4573f3e0cc7a0da84ba5d80b9c40d354324fc632
Quan <quan.liu@mpimet.mpg.de>
2023-06-28 17:10:59 +0200

    plot together

--- src/plots/extrc_tsurf_scatter.py
+++ src/plots/extrc_tsurf_scatter.py
@@ -64,3 +64,107 @@ def scatter_plot(ext_counts, t_surf,  axes):
 
             axes[i, j].set_xlim(-1, 5)
 
+
+#%%
+# plot function, x-axis is the extreme events count, y-axis is the pressure level
+# vertical line, and fill_betweenx the confidence interval
+def plot_extreme_count(ext_count, ax=None, label=None, colored=False):
+    """
+    plot the vertical profile of extreme event count
+    """
+    color = None
+    style = None
+    if colored:
+        if label == "first10":
+            color = "#1f77b4"
+            style = color
+        elif label == "last10":
+            color = "#ff7f0e"
+            style = color
+    else:
+        if label == "first10":
+            color = "gray"
+            style = "k-"
+        elif label == "last10":
+            color = "gray"
+            style = "k--"
+
+    if ax is None:
+        ax = plt.gca()
+
+    y = ext_count.plev / 100
+    true = ext_count.sel(confidence="true").values
+    low = ext_count.sel(confidence="low").values
+    high = ext_count.sel(confidence="high").values
+
+    # plot the confidence interval
+    ax.fill_betweenx(y, low, high, color=color, alpha=0.3)
+    # plot the true value
+    line = ax.plot(true, y, style, linewidth=1, label=label)
+    return line
+
+
+# %%
+def extreme_count_profile(first_count, last_count, colored=False, **kwargs):
+    """
+    plot the extreme event count profile for the NAO and EA,
+    and positive and negative extreme events
+    """
+    # parameters from kwargs
+    xlim = kwargs.pop("xlim", None)
+    
+    fig = pplt.figure(
+        # space=0,
+        refwidth="20em",
+    )
+    axes = fig.subplots(nrows=2, ncols=2)
+    axes.format(
+        abc=True,
+        abcloc="ul",
+        abcstyle="a",
+        xlabel="Extreme event count",
+        ylabel="Pressure level (hPa)",
+        suptitle="Extreme event count profile",
+        ylim=(1000, 200),
+        xminorticks="null",
+        yminorticks="null",
+        grid=False,
+        toplabels=("pos", "neg"),
+        leftlabels=("NAO", "EA"),
+        xlocator=20,
+    )
+
+    # plot the extreme event count profile
+    labels = ["first10", "last10"]
+    # the default color of matplotlib
+
+    for i, extreme_count in enumerate([first_count, last_count]):
+        plot_extreme_count(
+            extreme_count.sel(mode="NAO", extr_type="pos"),
+            axes[0, 0],
+            label=labels[i],
+            colored=colored,
+        )
+        plot_extreme_count(
+            extreme_count.sel(mode="NAO", extr_type="neg"),
+            axes[0, 1],
+            label=labels[i],
+            colored=colored,
+        )
+
+        plot_extreme_count(
+            extreme_count.sel(mode="EA", extr_type="pos"),
+            axes[1, 0],
+            label=labels[i],
+            colored=colored,
+        )
+        plot_extreme_count(
+            extreme_count.sel(mode="EA", extr_type="neg"),
+            axes[1, 1],
+            label=labels[i],
+            colored=colored,
+        )
+    for ax in axes:
+        ax.set_xlim(xlim)
+    # add legend
+    axes[0, 0].legend(loc="lr", ncols=1, frame=True)
