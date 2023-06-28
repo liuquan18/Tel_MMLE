aa75595ec251e91f6f9a755e94b2a8c68c6f5c93
Quan <quan.liu@mpimet.mpg.de>
2023-06-28 12:01:30 +0200

    as functions

--- script/8season/1index_generator_500hpa.py
+++ script/8season/1index_generator_500hpa.py
@@ -5,13 +5,12 @@ import xarray as xr
 import numpy as np
 import cartopy.crs as ccrs
 import src.compute.slurm_cluster as scluster
+import concurrent.futures
+
 #%%
 import importlib
 importlib.reload(index_generate)
 importlib.reload(scluster)
-
-#%%
-
 
 # %%
 # function for generate the index
@@ -22,106 +21,43 @@ def index_gen(model,fixedPattern,plev = 50000,season = 'MJJA',standard = 'first'
 
 # for different models
 #%%
-import concurrent.futures
-
-with concurrent.futures.ProcessPoolExecutor() as executor:
-    futures = [
-        executor.submit(index_gen, 'MPI_GE_onepct', 'decade', plev=50000),
-        executor.submit(index_gen, 'MPI_GE', 'decade', plev=50000),
-        executor.submit(index_gen, 'CanESM2', 'decade', plev=50000),
-        executor.submit(index_gen, 'CESM1_CAM5', 'decade', plev=50000),
-        executor.submit(index_gen, 'MK36', 'decade', plev=50000),
-        executor.submit(index_gen, 'GFDL_CM3', 'decade', plev=50000)
-    ]
-    for future in concurrent.futures.as_completed(futures):
-        try:
-            result = future.result()
-        except Exception as e:
-            print(f"Exception: {e}")
+def index_gen_models():
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        futures = [
+            executor.submit(index_gen, 'MPI_GE_onepct', 'decade', plev=50000),
+            executor.submit(index_gen, 'MPI_GE', 'decade', plev=50000),
+            executor.submit(index_gen, 'CanESM2', 'decade', plev=50000),
+            executor.submit(index_gen, 'CESM1_CAM5', 'decade', plev=50000),
+            executor.submit(index_gen, 'MK36', 'decade', plev=50000),
+            executor.submit(index_gen, 'GFDL_CM3', 'decade', plev=50000)
+        ]
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                result = future.result()
+            except Exception as e:
+                print(f"Exception: {e}")
 
 # for different seasons and standards, only for MPI_GE_onepct
 #%%
-import concurrent.futures
+def index_gen_seasons():
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        futures = [
+            executor.submit(index_gen, 'MPI_GE_onepct', 'decade', plev=50000, season='JJAS', standard='first'),
+            executor.submit(index_gen, 'MPI_GE_onepct', 'decade', plev=50000, season='MAM', standard='first'),
+            # executor.submit(index_gen, 'MPI_GE_onepct', 'decade', plev=50000, season='DJFM', standard='first'),
+            executor.submit(index_gen, 'MPI_GE_onepct', 'decade', plev=50000, season='JJAS', standard='temporal_ens'),
+            # executor.submit(index_gen, 'MPI_GE_onepct', 'decade', plev=50000, season='DJFM', standard='temporal_ens')
+            executor.submit(index_gen, 'MPI_GE_onepct', 'decade', plev=50000, season='MAM', standard='temporal_ens')
+        ]
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                result = future.result()
+            except Exception as e:
+                print(f"Exception: {e}")
 
-with concurrent.futures.ProcessPoolExecutor() as executor:
-    futures = [
-        executor.submit(index_gen, 'MPI_GE_onepct', 'decade', plev=50000, season='JJAS', standard='first'),
-        executor.submit(index_gen, 'MPI_GE_onepct', 'decade', plev=50000, season='MAM', standard='first'),
-        # executor.submit(index_gen, 'MPI_GE_onepct', 'decade', plev=50000, season='DJFM', standard='first'),
-        executor.submit(index_gen, 'MPI_GE_onepct', 'decade', plev=50000, season='JJAS', standard='temporal_ens'),
-        # executor.submit(index_gen, 'MPI_GE_onepct', 'decade', plev=50000, season='DJFM', standard='temporal_ens')
-        executor.submit(index_gen, 'MPI_GE_onepct', 'decade', plev=50000, season='MAM', standard='temporal_ens')
-    ]
-    for future in concurrent.futures.as_completed(futures):
-        try:
-            result = future.result()
-        except Exception as e:
-            print(f"Exception: {e}")
-
-
-
-# %%
-# CanESM2
-index_gen('CanESM2', 'decade', plev=50000)
-
+# define main funtion, run index_gen_seasons() or index_gen_models() here
 #%%
-index_gen('CanESM2','all')
-# %%
-# CESM1_CAM5
-index_gen('CESM1_CAM5','decade')
-index_gen('CESM1_CAM5','all')
-# %%
-# MK3.6
-index_gen('MK36','decade')
-index_gen('MK36','all')
-#%%
-# GFDL_CM3
-index_gen('GFDL_CM3','decade')
-index_gen('GFDL_CM3','all')
-
-#%%
-index_gen('MPI_GE','decade')
-index_gen('MPI_GE','all')
-
-# %%
-def read_eof(model,fixedPattern):
-    eof_dir = f"/work/mh0033/m300883/Tel_MMLE/data/{model}/EOF_result/plev_50000_{fixedPattern}_temporal_ens_eof_result.nc"
-    eof_result = xr.open_dataset(eof_dir)
-    return eof_result
-#%%
-# MPI_GE_onepct
-MPI_GE_decade_eof = read_eof('MPI_GE_onepct','decade')
-
-# %%
-Can_decade_eof = read_eof('CanESM2','decade')
-Can_all_eof = read_eof('CanESM2','all')
-# %%
-# CESM1_CAM5
-CESM_decade_eof = read_eof('CESM1_CAM5','decade')
-CESM_all_eof = read_eof('CESM1_CAM5','all')
-# %%
-# MK3.6
-MK36_decade_eof = read_eof('MK36','decade')
-MK36_all_eof = read_eof('MK36','all')
-# %%
-# GFDL_CM3
-GFDL_decade_eof = read_eof('GFDL_CM3','decade')
-GFDL_all_eof = read_eof('GFDL_CM3','all')
-# %%
-# plot the GFDL_CM3_decade EOF with the projection of projection=ccrs.Orthographic(-20,60)
-def quick_plot(GFDL_decade_eof):
-    GFDL_decade_eof.eof.sel(decade="1920-12-31",method = 'nearest').squeeze().plot.contourf(col = 'mode',
-    subplot_kws=dict(projection=ccrs.Orthographic(-20,60)),
-    levels=np.arange(-1.5,1.6,0.3),
-    transform=ccrs.PlateCarree(),
-)
-
-#%%
-quick_plot(GFDL_decade_eof)
-# %%
-quick_plot(Can_decade_eof)
-# %%
-quick_plot(CESM_decade_eof)
-# %%
-quick_plot(MK36_decade_eof)
-# %%
+def main():
+    index_gen_seasons()
+    # index_gen_models()
+    
