dd2b9618149f2f2e6b54f3212ae9d8fdd0600df1
Quan <quan.liu@mpimet.mpg.de>
2023-07-28 15:00:15 +0200

    done

--- script/9monthly/5month_extrc_diagram.py
+++ script/9monthly/5month_extrc_diagram.py
@@ -2,10 +2,17 @@
 import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
-import proplot as pplt
+
+import src.plots.month_extrc_dig as month_extrc_dig
+from scipy.stats import linregress
+
+
+#%%
+import importlib
+importlib.reload(month_extrc_dig)
 
 # %%
-def read_extrc(month, model="MPI_GE_onepct"):
+def read_extrc_month(month, model="MPI_GE_onepct"):
     dir = f"/work/mh0033/m300883/Tel_MMLE/data/{model}/extreme_count/"
     filename = f"plev_50000_decade_first_{month}_extre_counts.nc"
     ds = xr.open_dataset(dir + filename).sel(confidence="true")
@@ -13,8 +20,9 @@ def read_extrc(month, model="MPI_GE_onepct"):
     return ds.pc
 
 
-def read_extrc_month(model="MPI_GE_onepct"):
+def read_extrc(model="MPI_GE_onepct"):
     months = [
+        "Dec", # start from Dec
         "Jan",
         "Feb",
         "Mar",
@@ -26,186 +34,98 @@ def read_extrc_month(model="MPI_GE_onepct"):
         "Sep",
         "Oct",
         "Nov",
-        "Dec",
     ]
     mon_idx = xr.IndexVariable("month", months)
-    ds = [read_extrc(month, model) for month in months]
+    ds = [read_extrc_month(month, model) for month in months]
     ds = xr.concat(ds, dim=mon_idx)
     return ds
 
+def read_tsurf(model="MPI_GE_onepct"):
+    dir = f"/work/mh0033/m300883/Tel_MMLE/data/{model}/extreme_count/"
+    filename = f"ens_fld_year_mean.nc"
+    ds = xr.open_dataset(dir + filename)
+    # ds drop all the dimensions except time
+    ds = ds.reset_coords(drop=[coord for coord in ds.coords if coord != "time"])
+    ds["time"] = ds["time.year"]
+    try:
+        ds = ds.tsurf
+    except AttributeError:
+        try: 
+            ds = ds.ts
+        except AttributeError:
+            ds = ds.tas
+    return ds
 
-#%%
-# MPI_GE_onepct
-MPI_GE_onepct = read_extrc_month("MPI_GE_onepct")
-MPI_GE_onepct = MPI_GE_onepct.isel(time=[0, -1])
 # %%
 # MMLEA
 models = ["MPI_GE", "CanESM2", "CESM1_CAM5", "MK36", "GFDL_CM3"]
 models_ind = xr.IndexVariable("model", models)
-months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-]
-
-MMLEA = [read_extrc_month(model) for model in models]
-MMLEA = xr.concat(MMLEA, dim=models_ind)
-MMLEA = MMLEA.sel(time=slice("1950-01-01", "2091-01-01"))
 
 #%%
-# Calculate the difference between the first and last decade
-MMLEA_diff = MMLEA.isel(time=-1) - MMLEA.isel(time=0)
-
+MMLEA = [read_extrc(model) for model in models]
+MMLEA = xr.concat(MMLEA, dim=models_ind)
+MMLEA = MMLEA.sel(time=slice("1950-01-01", "2091-01-01"))
 #%%
 # Calculate the slope of the linear regression along the 'time' dimension
 MMLEA_slope = MMLEA.polyfit(dim="time", deg=1).polyfit_coefficients.sel(degree=1)
 
-# %%
-def bar_hatch():
-    pass
+#%%
+Tsurf = [read_tsurf(model) for model in models]
+Tsurf = xr.concat(Tsurf, dim=models_ind)
+Tsurf = Tsurf.sel(time=slice("1950-01-01", "2091-01-01"))
 
-gs = pplt.GridSpec(nrows=1, ncols=2)
-fig = pplt.figure(refwidth=2.2, span=False, share="labels")
-# the right order of the models
-models = ["MPI_GE", "CanESM2", "CESM1_CAM5", "MK36", "GFDL_CM3"]
-colors_model = ["C1", "tab:blue", "tab:purple", "tab:cyan", "C4"]
-model_color = dict(zip(models, colors_model))
-months = [
-    "Nov",
-    "Oct",
-    "Sep",
-    "Aug",
-    "Jul",
-    "Jun",
-    "May",
-    "Apr",
-    "Mar",
-    "Feb",
-    "Jan",
-    "Dec",
-]
+#%%
+def reg_slope(tsurf, extrc):
+    slope, _, _, _, _ = linregress(tsurf, extrc)
+    return slope
 
-
-for r, mode in enumerate(["NAO"]):
-    for c, extr_type in enumerate(["pos", "neg"]):
-        ax = fig.subplot(gs[r, c])
-
-        for i, month in enumerate(months):
-            data_month = MMLEA_slope.sel(mode=mode, extr_type=extr_type, month=month)
-            data_cum = data_month.cumsum(axis=0).values
-            for j, model in enumerate(models):
-                data_model = data_month.sel(model=model).values
-                bar_width = data_model
-                starts = data_cum[j] - bar_width
-                hatch = None
-                zorder = 0
-                if j > 0 and abs(data_cum[j]) < max(abs(data_cum[:j])):
-                    hatch = "/////"
-                    zorder = 10
-                bar = ax.barh(
-                    i,
-                    bar_width,
-                    fc=colors_model[j],
-                    edgecolor="blue9",
-                    label=model,
-                    left=starts,
-                    hatch = hatch,
-                    width = 1.6,
-                    alpha = 1,
-                    zorder = zorder,
-                )
-
-                ax.format(
-                    title=f"{mode} {extr_type}",
-                    xminorticks="null",
-                    yminorlocator="null",
-                    grid=False,
-                    xlim=(-0.4, 0.6),
-                    ylim=(12, -1),
-                    yticks=np.arange(11, -1, -1),
-                    yticklabels=months,
-                )
-
-fig.legend(bar, ncols=5, loc="b", frame=False)
-
-
-# fig.savefig("/work/mh0033/m300883/Tel_MMLE/docs/source/plots/monthly/month_extrc_diagram.png")
+Regress_slope = xr.apply_ufunc(
+    reg_slope,
+    Tsurf,
+    MMLEA,
+    input_core_dims=[["time"], ["time"]],
+    output_core_dims=[[]],
+    vectorize=True,
+    dask="parallelized",
+    output_dtypes=[float],
+)
 
 
 # %%
-def bar_diff():
+importlib.reload(month_extrc_dig)
 
-    gs = pplt.GridSpec(nrows=1, ncols=2)
-    fig = pplt.figure(refwidth=2.2, span=False, share="labels")
-    # the right order of the models
-    models = ["MPI_GE", "CanESM2", "CESM1_CAM5", "MK36", "GFDL_CM3"]
-    colors_model = ["C1", "tab:blue", "tab:purple", "tab:cyan", "C4"]
-    model_color = dict(zip(models, colors_model))
-    months = [
-        "Nov",
-        "Oct",
-        "Sep",
-        "Aug",
-        "Jul",
-        "Jun",
-        "May",
-        "Apr",
-        "Mar",
-        "Feb",
-        "Jan",
-        "Dec",
-    ]
+month_extrc_dig.bar_hatch(MMLEA_slope)
 
+#%%
+plt.savefig(
+    "/work/mh0033/m300883/Tel_MMLE/docs/source/plots/monthly/month_extrc_diagram_patch_time.png"
+)
+# %%
+month_extrc_dig.bar_side(MMLEA_slope)
+plt.savefig(
+    "/work/mh0033/m300883/Tel_MMLE/docs/source/plots/monthly/month_extrc_diagram_patch_side_time.png"
+)
+# %%
+month_extrc_dig.bar_hatch(Regress_slope,xlim = (-10,18))
+plt.savefig(
+    "/work/mh0033/m300883/Tel_MMLE/docs/source/plots/monthly/month_extrc_diagram_patch_tsurf.png"
+)
+# %%
+month_extrc_dig.bar_side(Regress_slope,xlim = (-5,15))
+plt.savefig(
+    "/work/mh0033/m300883/Tel_MMLE/docs/source/plots/monthly/month_extrc_diagram_patch_side_tsurf.png"
+)
+# %%
+importlib.reload(month_extrc_dig)
 
-
-    for r, mode in enumerate(["NAO"]):
-        for c, extr_type in enumerate(["pos", "neg"]):
-            ax = fig.subplot(gs[r, c])
-
-            for i, month in enumerate(months):
-                data_month = MMLEA_slope.sel(mode=mode, extr_type=extr_type, month=month)
-                # sort data_month by its value
-                data_month = data_month.sortby(data_month)
-                data_cum = data_month.cumsum(axis=0).values
-                for j, model in enumerate(data_month.model.values):
-                    data_model = data_month.sel(model=model).values
-                    bar_width = data_model
-                    start = data_cum[j] - bar_width
-                    hatch = None
-                    if j > 0 and abs(data_cum[j]) < abs(data_cum[j - 1]):
-                        hatch = "/////"
-                    bar = ax.barh(
-                        i,
-                        bar_width,
-                        fc=model_color[model],
-                        edgecolor="blue9",
-                        label=model,
-                        left=start,
-                        hatch = hatch,
-                        width = 1.6,
-
-                    )
-
-                    ax.format(
-                        title=f"{mode} {extr_type}",
-                        xminorticks="null",
-                        yminorlocator="null",
-                        grid=False,
-                        xlim=(-0.4, 0.6),
-                        ylim=(12, -1),
-                        yticks=np.arange(11, -1, -1),
-                        yticklabels=months,
-                    )
-
-    fig.legend(bar, ncols=5, loc="b", frame=False)
-
+#%%
+month_extrc_dig.bar_dual(MMLEA_slope)
+plt.savefig(
+    "/work/mh0033/m300883/Tel_MMLE/docs/source/plots/monthly/month_extrc_diagram_patch_dual_time.png"
+)
+# %%
+month_extrc_dig.bar_dual(Regress_slope,xlim = (-10,18))
+plt.savefig(
+    "/work/mh0033/m300883/Tel_MMLE/docs/source/plots/monthly/month_extrc_diagram_dual_tsurf.png"
+)
 # %%
