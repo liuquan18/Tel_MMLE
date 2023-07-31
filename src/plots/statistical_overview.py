d0a7a484cd194d5e8f191c20c06795d45c841907
Quan <quan.liu@mpimet.mpg.de>
2023-07-31 16:24:47 +0200

    support monthly

--- src/plots/statistical_overview.py
+++ src/plots/statistical_overview.py
@@ -22,13 +22,8 @@ warnings.filterwarnings("ignore")
 # colums for 'spatial map','pc hist','violion vertical profile'.
 
 
-def split_pc(first_pc, last_pc, mode):
-    # split the pc into two parts
+def index_to_df(first_pc, last_pc):
     periods = xr.IndexVariable("periods", ["first", "last"])
-
-    first_pc = first_pc.sel(mode=mode)
-    last_pc = last_pc.sel(mode=mode)
-
     both = xr.concat([first_pc, last_pc], dim=periods)
     both = both.to_dataframe().reset_index()
     both = both[["periods", "pc"]]
@@ -38,8 +33,8 @@ def split_pc(first_pc, last_pc, mode):
 def stat_overview(
     first_pc,
     last_pc,
-    first_fra = None,
-    last_fra = None,
+    first_fra=None,
+    last_fra=None,
     first_eof=None,
     last_eof=None,
     levels=np.arange(-2, 2.1, 0.4),
@@ -65,74 +60,33 @@ def stat_overview(
 
     for i, mode in enumerate(modes):
         # data preparation
-        ## pc to dataframe
-        df_500 = split_pc(first_pc, last_pc, mode)
 
         ## eof as xr.DataArray
-        first_eof_500 = first_eof.sel(mode=mode).squeeze()
-        first_fra_500 = first_fra.sel(mode=mode).squeeze()
+        first_eof_mode = first_eof.sel(mode=mode).squeeze()
+        first_fra_mode = first_fra.sel(mode=mode).squeeze()
 
         try:
-            last_eof_500 = last_eof.sel(mode=mode).squeeze()
-            last_fra_500 = last_fra.sel(mode=mode).squeeze()
+            last_eof_mode = last_eof.sel(mode=mode).squeeze()
+            last_fra_mode = last_fra.sel(mode=mode).squeeze()
         except AttributeError:
             pass
 
         # plot spatial map at 500hPa
-        spatial_ax = fig.add_subplot(
-            gs[i, 0], proj="ortho", proj_kw={"lon_0": -20, "lat_0": 60}
+        spatial_ax, fmap, lmap = spatial_pattern_plot(
+            fig,
+            gs[i, 0],
+            first_eof_mode,
+            first_fra_mode,
+            last_eof_mode,
+            last_fra_mode,
+            levels,
         )
 
-        fmap = first_eof_500.plot.contourf(
-            ax=spatial_ax, levels=levels, extend="both", add_colorbar=False
-        )
-        if last_eof is not None:
-            lmap = last_eof_500.plot.contour(
-                ax=spatial_ax,
-                colors="gray8",
-                nozero=True,
-                labels=True,
-                levels=np.delete(levels, int((len(levels) - 1) / 2)),
-                labels_kw={"weight": "bold"},
-                add_colorbar=False,
-            )
-
-            spatial_ax.format(
-                lonlines=20,
-                latlines=30,
-                coast=True,
-                coastlinewidth=0.5,
-                coastcolor="charcoal",
-                title=mode + f"({first_fra_500:.0%}" + f"->{last_fra_500:.0%})",
-            )
-        else:
-            spatial_ax.format(
-                lonlines=20,
-                latlines=30,
-                coast=True,
-                coastlinewidth=0.5,
-                coastcolor="charcoal",
-                title=mode + f"({first_fra_500:.0%})",
-            )
         # plot pc hist
-        hist_ax = fig.add_subplot(gs[i, 1])
-
-        hist = sns.histplot(
-            data=df_500,
-            x="pc",
-            hue="periods",
-            hue_order=["first", "last"],
-            palette=["#1f77b4", "#ff7f0e"],
-            multiple="dodge",
-            shrink=0.6,
-            bins=np.arange(-4, 4.1, 0.5),
-            legend=False,
-            ax=hist_ax,
-        )
-
-        hist_ax.format(grid=False, yminorticks="null", xminorticks="null", title=mode)
-        hist_ax.spines["right"].set_visible(False)
-        hist_ax.spines["top"].set_visible(False)
+        # split the pc into two parts
+        first_pc_mode = first_pc.sel(mode=mode)
+        last_pc_mode = last_pc.sel(mode=mode)
+        hist_ax = index_distribution_plot(fig, gs[i,1], first_pc_mode, last_pc_mode)
 
         # add legend
         f_patch = mpatches.Patch(color="#1f77b4", label="first10")
@@ -147,3 +101,135 @@ def stat_overview(
 
 
 # %%
+def spatial_pattern_plot(
+    fig,
+    gs,
+    first_eof,
+    first_fra,
+    last_eof=None,
+    last_fra=None,
+    levels=np.arange(-2, 2.1, 0.4),
+):
+    # plot spatial map at 500hPa
+    spatial_ax = fig.add_subplot(gs, proj="ortho", proj_kw={"lon_0": -20, "lat_0": 60})
+
+    fmap = first_eof.plot.contourf(
+        ax=spatial_ax, levels=levels, extend="both", add_colorbar=False
+    )
+
+    lmap = None
+    if last_eof is not None:
+        lmap = last_eof.plot.contour(
+            ax=spatial_ax,
+            colors="gray8",
+            nozero=True,
+            labels=True,
+            levels=np.delete(levels, int((len(levels) - 1) / 2)),
+            labels_kw={"weight": "bold"},
+            add_colorbar=False,
+        )
+
+        spatial_ax.format(
+            lonlines=20,
+            latlines=30,
+            coast=True,
+            coastlinewidth=0.5,
+            coastcolor="charcoal",
+            title=f"({first_fra:.0%}" + f"->{last_fra:.0%})",
+        )
+    else:
+        spatial_ax.format(
+            lonlines=20,
+            latlines=30,
+            coast=True,
+            coastlinewidth=0.5,
+            coastcolor="charcoal",
+            title=f"({first_fra_500:.0%})",
+        )
+    return spatial_ax, fmap, lmap
+
+
+
+def index_distribution_plot(fig, gs, first_pc, last_pc):
+    hist_ax = fig.add_subplot(gs)
+    df = index_to_df(first_pc, last_pc)
+    hist = sns.histplot(
+        data=df,
+        x="pc",
+        hue="periods",
+        hue_order=["first", "last"],
+        palette=["#1f77b4", "#ff7f0e"],
+        multiple="dodge",
+        shrink=0.6,
+        bins=np.arange(-4, 4.1, 0.5),
+        legend=False,
+        ax=hist_ax,
+        stat= "density",
+    )
+
+    hist_ax.format(grid=False, 
+                   yminorticks="null", 
+                   xminorticks="null",
+                   xticks = [-3,-1.5,0,1.5,3])
+    hist_ax.spines["right"].set_visible(False)
+    hist_ax.spines["top"].set_visible(False)
+    return hist_ax,hist
+
+
+def spatial_index_MMLEA(eof_firsts, eof_lasts):
+    models = ["MPI_GE", "CanESM2", "CESM1_CAM5", "MK36", "GFDL_CM3"]
+    models_legend = [
+    "MPI-GE (100)",
+    "CanESM2 (50)",
+    "CESM1-CAM5 (40)",
+    "MK3.6 (30)",
+    "GFDL-CM3 (20)",
+    ]
+
+    fig = pplt.figure(space=0, refwidth="50em", wspace=3, hspace=3)
+
+    gs = pplt.GridSpec(
+    ncols=5,
+    nrows=2,
+    hratios=(
+        1.4,
+        1,
+    ),
+)
+
+# rows for spatial pattern and index, columns for different models
+    for c, model in enumerate(models):
+    # plot spatial pattern
+        spatial_ax, fmap, lmap = spatial_pattern_plot(
+        fig,
+        gs[0, c],
+        eof_firsts[c].eof,
+        eof_firsts[c].fra,
+        eof_lasts[c].eof,
+        eof_lasts[c].fra,
+    )
+
+    # plot index distribution
+        hist_ax,hist = index_distribution_plot(
+        fig,
+        gs[1, c],
+        eof_firsts[c].pc,
+        eof_lasts[c].pc,
+    )
+
+    # add legend
+        f_patch = mpatches.Patch(color="#1f77b4", label="first10")
+        l_patch = mpatches.Patch(color="#ff7f0e", label="last10")
+
+    spatial_ax.colorbar(fmap, loc="r", title="std", pad=2)
+    hist_ax.legend(
+    handles=[f_patch, l_patch], loc="r", title="periods", frameon=False,
+    ncols = 1,
+)
+    fig.format(
+    suptitle="spatial pattern and distribution of summer NAO at 500hpa",
+    sharex=False,
+    sharey=False,
+    bottomlabels = models_legend,
+)
+    return fig
