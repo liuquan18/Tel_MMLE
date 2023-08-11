60c47eecb81e3fd8b49c332b28e6a409e246ead3
Quan <quan.liu@mpimet.mpg.de>
2023-08-11 14:02:00 +0200

    rewrite composite module

--- src/composite/composite.py
+++ src/composite/composite.py
@@ -26,21 +26,31 @@ def extreme(
     return extreme
 
 
-def composite_reduce(
-    index: xr.DataArray, data: xr.DataArray, dim: str = "com",reduction = 'mean', **kwargs
+def reduce_var(
+    index: xr.DataArray,
+    data: xr.DataArray,
+    dim: str = "com",
+    reduction="mean",
+    bootstrap=False,
+    **kwargs
 ):
+    """select the var data based on the coordinates of extreme cases in index, and do the average along dim.
+    if bootstrap is True, then do the bootstrap resampling.
+    **Arguments**
+        *index* the index of NAO and EA extremes
+        *data* the ts or pr data to do average.
+        *dim* the dimension to do average.
+        *reduction* the method to do average, 'mean' or 'mean_same_number', 'mean_weighted'
+        *bootstrap* if True, do the bootstrap resampling.
+        *kwargs* the arguments for different reduction methods.for example, 'count' for 'mean_same_number'
+    **Return**
+        *composite_mean* the composite mean of the extreme cases.
     """
-    do composite mean of data given the index
-    **Argument**
-        *index* the index which declears the coords to do the composite analysis
-        *data* the data to be meaned or counted
-        *reduction* "mean" or "count"
-        *dim* along which dim to do the mean and count.
-    """
+
     if reduction == "mean":
         # get the data at the  coordinates
         sel_data = data.where(index)
-        composited = sel_data.mean(dim=dim)
+        composite_mean = sel_data.mean(dim=dim)
 
     elif reduction == "mean_same_number":
         # get the number of extremes to reduce from kwargs
@@ -52,38 +62,63 @@ def composite_reduce(
             index = index.sortby(index, ascending=False)
         elif index.attrs["extreme_type"] == "neg":
             index = index.sortby(index, ascending=True)
-        index = index.isel(com=slice(0, num)) # select the first {num} indexes
+        index = index.isel(com=slice(0, num))  # select the first {num} indexes
         sel_data = data.where(index)
-        composited = sel_data.mean(dim=dim)
-    elif reduction == 'mean_weighted':
+        composite_mean = sel_data.mean(dim=dim)
+
+    elif reduction == "mean_weighted":
+        sel_data = data.where(index)
         weights = index
-        sel_data = data.where(index)
-        composited = sel_data.weighted(weights).mean(dim=dim)
-    return composited
+        composite_mean = sel_data.weighted(weights).mean(dim=dim)
+
+    if bootstrap:
+        # get the 'com' random index with the shape sel_data.size['com'] and 1000 times
+        n_samples = sel_data.sizes[
+            dim
+        ]  # the resampled data is the same length as the original data
+        rng = np.random.default_rng(seed=12345)
+        sampled_index = rng.choice(n_samples, size=(n_samples, 1000), replace=True)
+        composite_mean = []
+        for i in range(1000):
+            sample = sel_data.isel(com=sampled_index[:, i])
+            composite_mean.append(sample.mean(dim=dim))
+
+        composite_mean = xr.concat(composite_mean, dim="bootstrap")
+
+    return composite_mean
 
 
-def extreme_composite(index, data, reduction="mean", dim="com", threshold=1.5, **kwargs):
+def extreme_composite(
+    index, data, reduction="mean", dim="com", threshold=1.5, bootstrap=False, **kwargs
+):
     """
-    the composite mean or count of data, in terms of different extreme type.
-
+    get the indexes for the extreme cases, and do the composite analysis.
     **Arguments**
-        *index* the from which the coordinates of
-        extreme neg or pos cases are determined.
-        *data* the field that are going to be selected and averaged.
+        *index* the index of NAO and EA 
+        *data* the data to do average
+        *reduction* the method to do average, 'mean' or 'mean_same_number', 'mean_weighted'
+        *dim* the dimension to do average.
+        *threshold* the threshold to identify extreme cases.
+        *bootstrap* if True, do the bootstrap resampling.
+        *kwargs* the arguments for different reduction methods.for example, 'count' for 'mean_same_number'
     **Return**
-        *extreme_composite* the mean field or counts of extreme cases.
+        *composite_mean* the composite mean of the extreme positive and negative cases.
     """
     Ext_composite = []
     extreme_type = xr.DataArray(["pos", "neg"], dims=["extr_type"])
     for extr_type in extreme_type.values:
-
         # get the coordinates of the extremes
         extr_index = extreme(index, extreme_type=extr_type, threshold=threshold)
         extr_index.attrs["extreme_type"] = extr_type
 
         # do composite analysis based on the extreme index
-        extr_composite = composite_reduce(
-            extr_index, data, reduction=reduction, dim=dim, **kwargs
+        extr_composite = reduce_var(
+            extr_index,
+            data,
+            reduction=reduction,
+            dim=dim,
+            bootstrap=bootstrap,
+            **kwargs
         )
         Ext_composite.append(extr_composite)
 
@@ -91,3 +126,134 @@ def extreme_composite(index, data, reduction="mean", dim="com", threshold=1.5, *
     Extreme_composite = xr.concat(Ext_composite, dim=extreme_type)
 
     return Extreme_composite
+
+
+def Tel_field_composite(
+    index: xr.DataArray,
+    data: xr.DataArray,
+    threshold: float = 1.5,
+    reduction="mean",
+    bootstrap=False,
+    count = None,
+):
+    """
+    composite mean maps of NAO and EA extremes.
+    **Arguments**
+        *index* the index of NAO and EA
+        *data* the original geopotential data.
+        *reduction* mean or count
+        *period* 'first10','last10','all'
+    **Return**
+        *compostie* the composite mean of the extreme cases of NAO and EA.
+    """
+
+    # Select the same time period
+    index_c = index.copy()  # make a copy of the original data
+    data_c = data.copy()
+
+    # select the same time period
+    data_c = data_c.sortby("time")
+    data_c = data_c.sel(time=index_c.time, method="nearest")
+    data_c["time"] = index_c.time  # make sure the time is the same to use where function
+
+    # combine time and ens into one dim
+    index_c = index_c.stack(com=("time", "ens"))
+    data_c = data_c.stack(com=("time", "ens"))
+
+    # since there is 'mode' dim in index, here groupby.
+    tel_composite = index_c.groupby("mode").apply(
+        extreme_composite,
+        data=data_c,
+        reduction=reduction,
+        dim="com",
+        threshold=threshold,
+        bootstrap=bootstrap,
+        count = count,
+    )
+
+    return tel_composite
+
+
+def first_last_extreme_composite(
+    first_index: xr.DataArray,
+    last_index: xr.DataArray,
+    var_data: xr.DataArray,
+    threshold: float = 1.5,
+    reduction: str = "mean",
+    return_diff: bool = True,
+    **kwargs
+):
+    """
+    composite mean maps of first and last 10 years of field in terms of teleconnection mode extremes.
+    **Arguments**
+        *first_index* the index of first 10 years of NAO and EA
+        *last_index* the index of last 10 years of NAO and EA
+        *var_data* the ts or pr data.
+        *reduction* the method to do the reduction, 'mean' or 'mean_same_number', 'mean_weighted'
+        *return_diff* if True, return the difference between the first and last composite, and the significance.
+    **Return**
+        *composite* the composite mean of the extreme cases of NAO and EA for the first and last 10 years, and the difference between them.
+    """
+    count = kwargs.get('count', None)    # Select the same time period
+    first_index_c = first_index.copy()  # make a copy of the original data
+    last_index_c = last_index.copy()
+    var_data_first = var_data.copy()
+    var_data_last = var_data.copy()
+
+    # first 10 years
+    first_composite = Tel_field_composite(
+        first_index_c,
+        var_data_first,
+        threshold=1.5,
+        reduction="mean",
+        bootstrap=False,
+        count = count,
+    )
+
+    # last 10 years
+    last_composite = Tel_field_composite(
+        last_index_c,
+        var_data_last,
+        threshold=threshold,
+        reduction=reduction,
+        bootstrap=False,
+        count = count,
+    )
+
+    # combine the first and last composite
+    period = xr.IndexVariable('period', ['first', 'last'])
+    composite = xr.concat([first_composite, last_composite], dim=period)
+    diff = first_composite - last_composite
+
+    if return_diff:
+        # first 10 years with bootstrap
+        first_composite_boot = Tel_field_composite(
+            first_index_c,
+            var_data_first,
+            threshold=1.5,
+            reduction="mean",
+            bootstrap=True,
+        )
+
+        # last 10 years
+        last_composite_boot = Tel_field_composite(
+            last_index_c,
+            var_data_last,
+            threshold=threshold,
+            reduction=reduction,
+            bootstrap=True,
+        )
+
+        # difference between first and last 10 years
+        diff_boot = first_composite_boot - last_composite_boot
+
+        # check if the difference is significant
+        # get the 95% confidence interval
+        ci = diff_boot.quantile([0.025, 0.975], dim="bootstrap")
+        # check if 0 is in the interval, return boolean
+        diff_sig = xr.where((ci.sel(quantile=0.975) > 0) & (ci.sel(quantile=0.025) < 0), 1, 0)
+        # combine the first, last, diff and diff_sig together
+        period = xr.IndexVariable('period', ['first', 'last', 'diff', 'diff_sig'])
+        composite = xr.concat([first_composite, last_composite, diff, diff_sig], dim=period)
+
+    return composite
