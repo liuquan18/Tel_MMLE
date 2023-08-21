48fd7973b9af5c0e4a2623d02eab98572a7e3d20
Quan <quan.liu@mpimet.mpg.de>
2023-08-21 16:02:29 +0200

    ifno

--- script/10_JJA_season_month/composite_analysis_submitter.sh
+++ script/10_JJA_season_month/composite_analysis_submitter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=com_ci
+#SBATCH --job-name=com_zg
 #SBATCH --time=08:00:00
 #SBATCH --partition=compute
 #SBATCH --nodes=1
@@ -7,10 +7,10 @@
 #SBATCH --mem=0
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
-#SBATCH --output=com_ci.%j.out
+#SBATCH --output=com_zg.%j.out
 
 
-mpirun -np 2 python -u composite_analysis.py $1 $2 $3
+mpirun -np 1 python -u composite_analysis.py $1 $2 $3
 # python -u hello0index_generator.py $1 $2 $3
 
 # srun -n 2 --mpi=pmi2 env MPICC=/sw/spack-levante/openmpi-4.1.2-yfwe6t/bin/mpicc python -m mpi4py hello1index_generator_500hpa.py
