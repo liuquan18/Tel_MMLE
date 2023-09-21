cd460e045fd82a3f0fe7cf8c90522681fb3640ce
Quan <quan.liu@mpimet.mpg.de>
2023-09-21 11:55:11 +0200

    allow to keep ensmean

--- src/MMLE_TEL/index_stats.py
+++ src/MMLE_TEL/index_stats.py
@@ -88,7 +88,7 @@ def read_spmean_tsurf(tsurf_dir):
     return tsurf_arr
 
 
-def read_var_data(field_var_dir,remove_ens_mean=True):
+def read_var_data(field_var_dir,remove_ensmean=True):
     print(" reading the field data...")
 
     all_ens_lists = sorted(
@@ -103,7 +103,7 @@ def read_var_data(field_var_dir,remove_ens_mean=True):
         pass
 
     # demean the ensemble mean
-    if remove_ens_mean:
+    if remove_ensmean:
         var_data = var_data - var_data.mean(dim="ens")
     return var_data
 
