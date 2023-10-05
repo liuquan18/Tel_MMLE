0388d8f8b04eed11468ada17baa178b10c9906a5
Quan <quan.liu@mpimet.mpg.de>
2023-10-05 14:30:32 +0200

    submiiter

--- script/10_JJA_season_month/block_event_monthly_master_submitter.py
+++ script/10_JJA_season_month/block_event_monthly_master_submitter.py
@@ -12,6 +12,6 @@ njobs  = int(nsteps/npar) # 16
 for kk in range(njobs): #
     k1 = kk*npar #
     k2 = (kk+1)*npar #
-    os.system(f"sbatch ./block_event_month_submitter.sh {kk+1} {k1} {k2}") # 1,0,4, 2,4,8, 3,8,12
+    os.system(f"sbatch ./block_event_monthly_submitter.sh {kk+1} {k1} {k2}") # 1,0,4, 2,4,8, 3,8,12
 
 # %%
