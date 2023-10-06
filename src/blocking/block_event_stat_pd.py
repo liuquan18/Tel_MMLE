2e9f00f8ed703f17cb99cf52c1e23b7e6dff1a4b
Quan <quan.liu@mpimet.mpg.de>
2023-10-06 13:51:33 +0200

    correct time

--- src/blocking/block_event_stat_pd.py
+++ src/blocking/block_event_stat_pd.py
@@ -4,9 +4,10 @@ import pandas as pd
 
 
 # %%
-def annual_blocking_stat(ix, threshod=40):
+def annual_blocking_stat(ix, month,threshod=40):
     """
     seperate the year, lon, lat from the index of the xarray
+    *month* for chaning the time
     """
     # find the blocks
     df = ix.to_dataframe().reset_index()
@@ -14,16 +15,20 @@ def annual_blocking_stat(ix, threshod=40):
     G = df[df['IB index']>0].groupby([df.time.dt.year,'lat','lon'])['IB index']
     blocks = G.size()
     blocks = blocks.reset_index()
+    dates = pd.to_datetime([year + '-' + month + '-01' for year in blocks.time.values.astype(str)])
+    blocks['time'] = dates
 
     # duration
-    duration = blocks.groupby(['time','lat','lon'])['IB index'].apply(average_duration)
+    duration = blocks.groupby([blocks.time.dt.year,'lat','lon'])['IB index'].apply(average_duration)
     Duration = duration.to_xarray()
     Duration.name = 'duration'
+    Duration['time'] = pd.to_datetime([year + '-' + month + '-01' for year in Duration.time.values.astype(str)])
 
     # count of events that lasted more than 10 days
-    counts = blocks.groupby(['time','lat','lon'])['IB index'].apply(event_count,threshod=threshod)
+    counts = blocks.groupby([blocks.time.dt.year,'lat','lon'])['IB index'].apply(event_count,threshod=threshod)
     Count = counts.to_xarray()
     Count.name = 'event_count'
+    Count['time'] = pd.to_datetime([year + '-' + month + '-01' for year in Count.time.values.astype(str)])
 
     return Duration, Count
 
