bbdf77124279ab90bbd231469eb4ec9bc60bace5
Quan <quan.liu@mpimet.mpg.de>
2023-10-06 10:43:20 +0200

    seperate

--- src/blocking/block_event_stat.py
+++ src/blocking/block_event_stat.py
@@ -71,26 +71,3 @@ def decade_events_count(arr, threshold=40): # 10 days (6h a day)
     decade_count = arr.resample(time = '10AS').apply(count_spatial_applyer, threshold=threshold)
     return decade_count
 
-
-# %%
-def annual_blocking_stat(ix, threshod=40):
-    """
-    seperate the year, lon, lat from the index of the xarray
-    """
-    df = ix.to_dataframe().reset_index()
-    Grouper = df.groupby([df.time.dt.year, 'lat','lon'])['IB index'].transform(lambda x: (x<=0).cumsum())
-    G = df[df['IB index']>0].groupby([df.time.dt.year,'lat','lon'])['IB index']
-    # duration
-    duration = G.size()
-    Duration = duration.to_xarray()
-
-    # count of events that lasted more than 10 days
-    duration = duration.reset_index()
-    count = duration.groupby(['time','lat','lon'])['duration'].apply(count)
-    Count = count.to_xarray()
-
-    return Duration, Count
-
-def count(duration, threshod = 40):
-    count = duration[duration>threshod].count()
-    return count
