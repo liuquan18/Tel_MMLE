fcff8577de294ba5f0c70985bad16fb62aac17b9
Quan <quan.liu@mpimet.mpg.de>
2023-10-16 17:21:14 +0200

    rename

--- script/12ERA5_ens/daily_check.py
+++
@@ -1,8 +0,0 @@
-#%%
-import xarray as xr
-import numpy as np
-import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-# %%
-ex = xr.open_dataset("/work/mh0033/m300883/Tel_MMLE/data/ERA5_allens_daily/zg/download.nc")
-# %%
