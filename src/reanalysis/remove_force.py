ff640623c40d60cb02ac5cfa308326c0f7d9b8ab
Quan <quan.liu@mpimet.mpg.de>
2023-11-01 15:34:56 +0100

    ensmean and 0

--- src/reanalysis/remove_force.py
+++ src/reanalysis/remove_force.py
@@ -30,5 +30,47 @@ def detrend(data,method = 'linear_trend'):
         fitted = ens_data.groupby('time.month').apply(linear_trend)
     elif method == 'quadratic_trend':
         fitted = ens_data.groupby('time.month').apply(quadratic_trend)
+    elif method == 'ens_mean':
+        fitted = ens_data.mean(dim = 'ens')
+    elif method == None:
+        fitted = 0
     detrended = data - fitted
     return detrended
+#%%
+# read gph data
+def read_gph_data(model, external_forcing="quadratic_trend", **kwargs):
+    plev = 50000
+    odir = "/work/mh0033/m300883/Tel_MMLE/data/" + model + "/"
+    start_year = kwargs.get("start_year", "1940")
+    end_year = kwargs.get("end_year", "2022")
+
+    data_JJA = []
+    for month in ["Jun", "Jul", "Aug"]:
+        print(f"reading the gph data of {month} ...")
+        zg_path = odir + "zg_" + month + "/"
+        file_names = sorted(glob.glob(zg_path + "*.nc"))
+        
+
+        # if model contains 'all_ens'
+        if "allens" in model:
+            try:
+                data_month = xr.open_mfdataset(file_names, combine="nested", concat_dim="ens")
+                data_month = data_month['HGT']
+            except KeyError:
+                data_month = data_month['zg']
+        else:
+            if model == "CR20":
+                data_month = xr.open_dataset(file_names[0])
+                data_month = data_month.sel(level = plev/100)
+                data_month = data_month['hgt']
+            elif model == "ERA5":
+                data_month = xr.open_mfdataset(file_names, combine="by_coords")
+                data_month = data_month.sel(plev = plev)
+                data_month = data_month['var129']
+        data_month = data_month.sel(time=slice(start_year, end_year))
+        data_JJA.append(data_month)
+    data = xr.concat(data_JJA, dim="time").sortby("time")
+    # remove the forced trend
+    print("detrending ...")
+    detrended = detrend(data,method = external_forcing)
+    return detrended.squeeze()
