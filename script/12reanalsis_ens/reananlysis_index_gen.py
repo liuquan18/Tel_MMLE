27409886c35b87568fec1916f3f3f0427fab5d9e
Quan <quan.liu@mpimet.mpg.de>
2023-11-02 16:38:59 +0100

    workd

--- script/12reanalsis_ens/reananlysis_index_gen.py
+++ script/12reanalsis_ens/reananlysis_index_gen.py
@@ -88,7 +88,7 @@ def read_gph_data(model, external_forcing="quadratic_trend", **kwargs):
 
 
 # %%
-def decompose_period(xarr, nmode = 2):
+def decompose_period(xarr, nmode = 2,period = True):
     xarr = xarr.fillna(0) # fill nan with 0
     field = xarr.sortby("time")
     if 'ens' in xarr.dims:
@@ -96,7 +96,8 @@ def decompose_period(xarr, nmode = 2):
         dim = 'com'
     else:
         dim = 'time'
-    eof_result = ssp.doeof(field, standard="eof_spatial_std",nmode=nmode,dim = dim)
+    standard = 'eof_spatial_std' if period else 'pc_temporal_std'
+    eof_result = ssp.doeof(field, standard=standard,nmode=nmode,dim = dim)
     return eof_result
 
 
@@ -116,13 +117,14 @@ def standard_period(first_eof, last_eof):
 #%%
 
 class EOF_reanalysis:
-    def __init__(self, model, group_size=40, external_forcing="linear_trend", **kwargs):
+    def __init__(self, model, group_size=40, external_forcing="linear_trend", period_dec = False, **kwargs):
         self.model = model
         self.group_size = group_size
         self.external_forcing = external_forcing
         self.plev = 50000
         self.standard = "first"
         self.nmode = kwargs.get("nmode", 2)
+        self.period_dec = period_dec
 
         self.start_year = kwargs.get("start_year", "1940")
         self.end_year = kwargs.get("end_year", "2022")
@@ -130,17 +132,22 @@ class EOF_reanalysis:
         self.data = read_gph_data(model, external_forcing=external_forcing,
                                   start_year = self.start_year, end_year = self.end_year)
 
+        if self.period_dec: # period decomposition
+            self.first_data = self.data.sel(
+                time=slice(self.start_year, str(int(self.start_year) + self.group_size - 1))
+            )
+            self.last_data = self.data.sel(
+                time=slice(str(int(self.end_year) - self.group_size + 1), self.end_year)
+            )
 
-        self.first_data = self.data.sel(
-            time=slice(self.start_year, str(int(self.start_year) + self.group_size - 1))
-        )
-        self.last_data = self.data.sel(
-            time=slice(str(int(self.end_year) - self.group_size + 1), self.end_year)
-        )
-
-        print("********* decomposing *********")
-        self.first_eof = decompose_period(self.first_data, nmode=self.nmode)
-        self.last_eof = decompose_period(self.last_data, nmode=self.nmode)
+            print("********* period decomposing *********")
+            self.first_eof = decompose_period(self.first_data, nmode=self.nmode)
+            self.last_eof = decompose_period(self.last_data, nmode=self.nmode)
+        else:
+            print("********* all decomposing *********")
+            self.eof = decompose_period(self.data, nmode=self.nmode, period=False)
+            self.first_eof= self.eof.sel(time = slice(self.start_year, str(int(self.start_year) + self.group_size - 1)))
+            self.last_eof = self.eof.sel(time = slice(str(int(self.end_year) - self.group_size + 1), self.end_year))
 
         print("********* standardizing *********")
         self.first_eof_std, self.last_eof_std = standard_period(
@@ -163,7 +170,7 @@ class EOF_reanalysis:
             f"/work/mh0033/m300883/Tel_MMLE/data/{self.model}/EOF_result/last_{self.group_size}_eof_std.nc"
         )
 
-    def plot_spatial_index(self,save=False):
+    def plot_spatial_index(self,levels = np.arange(-2, 2.1, 0.4), save=False):
         
         fig2 = pplt.figure(figsize=(180 / 25.4, 90 / 25.4), sharex=False, sharey=False)
         fig2.format(
@@ -179,14 +186,22 @@ class EOF_reanalysis:
         ax2 = fig2.add_subplot(gs[1])
 
 
-        ax1, fmap, lmap = stat_overview.spatial_pattern_plot(
-            ax1,
-            self.first_eof_std.eof.sel(mode="NAO").squeeze(),
-            self.first_eof_std.fra.sel(mode="NAO").squeeze(),
-            self.last_eof_std.eof.sel(mode="NAO", ).squeeze(),
-            self.last_eof_std.fra.sel(mode="NAO").squeeze(),
-            levels=np.arange(-2, 2.1, 0.4),
-        )
+        if self.period_dec:
+            ax1, fmap, lmap = stat_overview.spatial_pattern_plot(
+                ax1,
+                self.first_eof_std.eof.sel(mode="NAO").squeeze(),
+                self.first_eof_std.fra.sel(mode="NAO").squeeze(),
+                self.last_eof_std.eof.sel(mode="NAO", ).squeeze(),
+                self.last_eof_std.fra.sel(mode="NAO").squeeze(),
+                levels=levels,
+            )
+        else:
+            ax1, fmap, lmap = stat_overview.spatial_pattern_plot(
+                ax1,
+                self.first_eof_std.eof.sel(mode="NAO").squeeze(),
+                self.first_eof_std.fra.sel(mode="NAO").squeeze(),
+                levels=levels,
+            )
         ax2, hist = stat_overview.index_distribution_plot(
             ax2,
             self.first_eof_std.pc.sel(mode="NAO"),
@@ -234,4 +249,6 @@ CR20_40.plot_spatial_index(save=True)
 CR20_allens_40 = EOF_reanalysis("CR20_allens", group_size=40, start_year="1950", end_year="2015")
 CR20_allens_40.save_eof()
 CR20_allens_40.plot_spatial_index(save=True)
+# %%
+CR20_40 = EOF_reanalysis("CR20", group_size=40, start_year="1850", end_year="2015",period_dec = False)
 # %%
