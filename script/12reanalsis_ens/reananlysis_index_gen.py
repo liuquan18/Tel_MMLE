a1646c74f405f07c4cc28b82e0d94fafa198eab7
Quan <quan.liu@mpimet.mpg.de>
2024-01-03 16:16:53 +0100

    using package

--- script/12reanalsis_ens/reananlysis_index_gen.py
+++ script/12reanalsis_ens/reananlysis_index_gen.py
@@ -22,70 +22,8 @@ import importlib
 importlib.reload(index_generate)
 importlib.reload(ssp)
 
-# %%
-def linear_trend(xarr):
-    linear_coef = xarr.polyfit(dim="time", deg=1)
-    linear_fitted = xr.polyval(xarr.time, linear_coef.polyfit_coefficients)
-    return linear_fitted
-def quadratic_trend(xarr):
-    quadratic_coef = xarr.polyfit(dim="time", deg=2)
-    quadratic_fitted = xr.polyval(xarr.time, quadratic_coef.polyfit_coefficients)
-    return quadratic_fitted
-
-def detrend(data,method = 'linear_trend'):
-    ens_data = data.copy()
-    try:
-        ens_data = ens_data.mean(dim = 'ens')
-    except ValueError:
-        pass
-    if method == 'linear_trend':
-        fitted = ens_data.groupby('time.month').apply(linear_trend)
-    elif method == 'quadratic_trend':
-        fitted = ens_data.groupby('time.month').apply(quadratic_trend)
-    elif method == 'ens_mean':
-        fitted = ens_data.mean(dim = 'ens')
-    detrended = data - fitted
-    return detrended
-
-# %%
-# read gph data
-def read_gph_data(model, external_forcing="quadratic_trend", **kwargs):
-    plev = 50000
-    odir = "/work/mh0033/m300883/Tel_MMLE/data/" + model + "/"
-    start_year = kwargs.get("start_year", "1940")
-    end_year = kwargs.get("end_year", "2022")
-
-    data_JJA = []
-    for month in ["Jun", "Jul", "Aug"]:
-        print(f"reading the gph data of {month} ...")
-        zg_path = odir + "zg_" + month + "/"
-        file_names = sorted(glob.glob(zg_path + "*.nc"))
-        
-
-        # if model contains 'all_ens'
-        if "allens" in model:
-            try:
-                data_month = xr.open_mfdataset(file_names, combine="nested", concat_dim="ens")
-                data_month = data_month['HGT']
-            except KeyError:
-                data_month = data_month['zg']
-        else:
-            if model == "CR20":
-                data_month = xr.open_dataset(file_names[0])
-                data_month = data_month.sel(level = plev/100)
-                data_month = data_month['hgt']
-            elif model == "ERA5":
-                data_month = xr.open_mfdataset(file_names, combine="by_coords")
-                data_month = data_month.sel(plev = plev)
-                data_month = data_month['var129']
-        data_month = data_month.sel(time=slice(start_year, end_year))
-        data_JJA.append(data_month)
-    data = xr.concat(data_JJA, dim="time").sortby("time")
-    # remove the forced trend
-    print("detrending ...")
-    detrended = detrend(data,method = external_forcing)
-    return detrended.squeeze()
-
+#%%
+from src.reanalysis.utils import read_gph_data
 
 # %%
 def decompose_period(xarr, nmode = 2,period = True):
@@ -254,5 +192,15 @@ CR20_allens_40 = EOF_reanalysis("CR20_allens", group_size=40, start_year="1850",
 CR20_allens_40.save_eof()
 CR20_allens_40.plot_spatial_index(save=True)
 # %%
-CR20_40 = EOF_reanalysis("CR20", group_size=40, start_year="1850", end_year="2015",period_dec = False)
+# period decomposition
+CR20_allens_40 = EOF_reanalysis("CR20_allens", group_size=40, start_year="1850", end_year="2015",external_forcing='quadratic_trend',period_dec = True)
+
+#%%
+CR20_allens_40.first_eof_std.to_netcdf(
+    f"/work/mh0033/m300883/Tel_MMLE/data/CR20_allens/EOF_result/periodic_first_40_eof_std.nc"
+)
+CR20_allens_40.last_eof_std.to_netcdf(
+    f"/work/mh0033/m300883/Tel_MMLE/data/CR20_allens/EOF_result/periodic_last_40_eof_std.nc"
+)
+
 # %%
