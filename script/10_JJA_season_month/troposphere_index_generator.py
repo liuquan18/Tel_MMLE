55dc6858ea7c994ce79c90a4f22156127b7af1cb
Quan <quan.liu@mpimet.mpg.de>
2024-03-22 15:27:21 +0100

    try dep decomposition vertically

--- script/10_JJA_season_month/troposphere_index_generator.py
+++ script/10_JJA_season_month/troposphere_index_generator.py
@@ -3,20 +3,24 @@
 import xarray as xr
 from src.MMLE_TEL.index_generator import decompose_troposphere
 import logging
+
+#%%
+import importlib
 # %%
 model = 'MPI_GE'
 # %%
-def index_gene(model):
+def index_gene(model, vertical_eof = 'dep'):
     GEN = decompose_troposphere(
         model = model,
         fixedPattern='decade',
         standard='first',
         season = 'JJA',
         all_years=False,
+        vertical_eof = vertical_eof,
     )
     
-    logging.info(f"Start saving {model}...")
+    logging.critical(f"Start saving {model}...")
     GEN.save_result()
 # %%
-index_gene(model)
+index_gene(model, vertical_eof = 'dep')
 # %%
