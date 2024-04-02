0cba66ca0e232b87ad92a43d37fa097f0c86f8f5
Quan <quan.liu@mpimet.mpg.de>
2024-04-02 11:02:53 +0200

    finish CR20 index generates

--- script/12reanalsis_ens/troposphere_reanalysis_index_gen.py
+++ script/12reanalsis_ens/troposphere_reanalysis_index_gen.py
@@ -13,10 +13,13 @@ from src.reanalysis.decompose import EOF_reanalysis
 # %%
 plev = int(sys.argv[1]) # get the plev from bash file
 model = str(sys.argv[2]) # get the model from bash file
+
+#%%
+variable = 'zg_all_plev' if model == 'CR20_allens' else 'zg'
 # %%
-CR20_all_HGT = EOF_reanalysis(model, group_size=40, start_year="1850", end_year="2015", plev = plev, variable = "zg_all_plev")
+CR20_all_HGT = EOF_reanalysis(model, group_size=40, start_year="1850", end_year="2015", plev = plev, variable = variable)
 # %%
-CR20_all_HGT.eof.to_netcdf(f"/work/mh0033/m300883/Tel_MMLE/data/CR20_allens/EOF_result/all_{plev}_eof.nc")
-CR20_all_HGT.first_eof.to_netcdf(f"/work/mh0033/m300883/Tel_MMLE/data/CR20_allens/EOF_result/first_plev{plev}_eof.nc")
-CR20_all_HGT.last_eof.to_netcdf(f"/work/mh0033/m300883/Tel_MMLE/data/CR20_allens/EOF_result/last_plev{plev}_eof.nc")
+CR20_all_HGT.eof.to_netcdf(f"/work/mh0033/m300883/Tel_MMLE/data/{model}/EOF_result/all_{plev}_eof.nc")
+CR20_all_HGT.first_eof.to_netcdf(f"/work/mh0033/m300883/Tel_MMLE/data/{model}/EOF_result/first_plev{plev}_eof.nc")
+CR20_all_HGT.last_eof.to_netcdf(f"/work/mh0033/m300883/Tel_MMLE/data/{model}/EOF_result/last_plev{plev}_eof.nc")
 # %%
