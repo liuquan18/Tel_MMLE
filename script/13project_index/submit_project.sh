f65fc878f23a0dc495daf6d0d4c681c3e0995455
Quan <quan.liu@mpimet.mpg.de>
2024-05-13 15:29:05 +0200

    submit sbatch

--- script/13project_index/submit_project.sh
+++ script/13project_index/submit_project.sh
@@ -8,7 +8,7 @@
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
 #SBATCH --output=project.%j.out
-
+source ~./bashrc
 module load python3/unstable
 conda activate /home/m/m300883/miniconda3/envs/TelSeason
 mpirun python -u project_index_single_model.py $1 $2 # node-index, model-name
