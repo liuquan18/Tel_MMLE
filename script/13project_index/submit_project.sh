8e95f609ab2e2e1593e5af4129502a0423605123
Quan <quan.liu@mpimet.mpg.de>
2024-05-13 11:25:08 +0200

    submit single mdoel with mpi4py

---
+++ script/13project_index/submit_project.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=project
+#SBATCH --time=08:00:00
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks=10
+#SBATCH --mem=0
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh0033
+#SBATCH --output=project.%j.out
+
+module load python3/unstable
+conda activate /home/m/m300883/miniconda3/envs/TelSeason
+mpirun python -u project_index_single_model.py $1 $2 # node-index, model-name
