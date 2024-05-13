e7b5be65906829b8c20bc5aeb7293d42a8b0a512
Quan <quan.liu@mpimet.mpg.de>
2024-05-13 09:39:16 +0200

    remove non-used imports

--- script/10_JJA_season_month/index_generator.py
+++ script/10_JJA_season_month/index_generator.py
@@ -1,10 +1,6 @@
 # %%
 import src.MMLE_TEL.index_generator as index_generate
-import xarray as xr
-import numpy as np
-import cartopy.crs as ccrs
 import sys
-from mpi4py import MPI
 import src.Teleconnection.rolling_eof as rolling_eof
 
 
