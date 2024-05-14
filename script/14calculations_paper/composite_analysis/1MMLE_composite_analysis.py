654a68ca864acb7a1080c0cb4cb25e41ea8e3a9c
Quan <quan.liu@mpimet.mpg.de>
2024-05-14 11:25:54 +0200

    move to composite analysis folder

---
+++ script/14calculations_paper/composite_analysis/1MMLE_composite_analysis.py
@@ -0,0 +1,136 @@
+# %%
+import numpy as np
+import importlib
+import matplotlib.pyplot as plt
+import src.plots.extreme_plot as extrc_tsurf
+import src.MMLE_TEL.index_stats as index_stats
+import xarray as xr
+import os
+import sys
+import src.composite as composite
+#%%
+import importlib
+
+importlib.reload(index_stats)
+importlib.reload(extrc_tsurf)
+importlib.reload(composite)
+
+#%%
+def read_var_months(model,var_name='ts',remove_ensmean = True):
+    odir = f'/work/mh0033/m300883/Tel_MMLE/data/{model}/'
+    Jun_fl = odir + f'{var_name}_Jun/'
+    Jul_fl = odir + f'{var_name}_Jul/'
+    Aug_fl = odir + f'{var_name}_Aug/'
+
+    Jun_f = index_stats.read_var_data(Jun_fl,remove_ensmean=remove_ensmean)
+    Jul_f = index_stats.read_var_data(Jul_fl,remove_ensmean=remove_ensmean)
+    Aug_f = index_stats.read_var_data(Aug_fl,remove_ensmean=remove_ensmean)
+
+    JJA_f = xr.concat([Jun_f, Jul_f, Aug_f], dim='time')
+    JJA_f = JJA_f.sortby('time')
+    if var_name == 'ts':
+        try:
+            JJA_f = JJA_f['tsurf']
+        except KeyError:
+            JJA_f = JJA_f['ts']
+    elif var_name == 'pr':
+        try:
+            JJA_f = JJA_f['pr']
+        except KeyError:
+            JJA_f = JJA_f['precip']
+    elif var_name == 'zg':
+        try:
+            JJA_f = JJA_f.var156
+        except AttributeError:
+            JJA_f = JJA_f.zg
+    elif var_name == 'u10':
+        JJA_f = JJA_f.u10
+    elif var_name == 'v10':
+        JJA_f = JJA_f.v10
+    elif var_name == 'bDays':
+        JJA_f = JJA_f['IB index']
+        JJA_f = JJA_f.drop('plev')
+
+    return JJA_f
+#%%
+def same_count(model,fixed_pattern = 'decade_mpi'):
+    """read extreme counts"""
+    print("reading the extreme counts ... ")
+    odir = "/work/mh0033/m300883/Tel_MMLE/data/" + model + "/extreme_count/"
+    filename = f"plev_50000_{fixed_pattern}_first_JJA_extre_counts.nc"
+    ds = xr.open_dataset(odir + filename).pc
+    count = ds.isel(time = 0).sel(confidence = 'true')
+    count = count.drop(['plev','confidence','time'])
+    return count
+#%%
+def composite(model,var_name='ts',reduction = 'mean',**kwargs):
+    remove_ensmean = kwargs.get('remove_ensmean',True)
+    var_data = read_var_months(model,var_name=var_name,remove_ensmean=remove_ensmean)
+    if var_name == 'zg':
+        var_data = var_data.sel(plev=50000).drop('plev')
+    count = same_count(model) if reduction == 'mean_same_number' else 'all'
+    index_stats.composite_analysis(
+        model            = model,
+        index_season     = 'JJA',
+        var_season     = 'JJA',
+        fixed_pattern    = 'decade_mpi',
+        var_data         = var_data,
+        var_name         = var_name,
+        reduction        = reduction,
+        count            = count,
+        )
+    
+#%%
+num = int(sys.argv[1])
+t1 = int(sys.argv[2])
+t2 = int(sys.argv[3])
+
+#%%
+def mean_all(num,rank):
+    models = ['MPI_GE_onepct','MK36','GFDL_CM3','CanESM2','CESM1_CAM5','MPI_GE']
+    vars = ['zg']
+
+    print("===========================================")
+    print(f"node_num:{num} is doing {models[num-1]}")
+    composite(models[num-1])
+
+    # for reduction = 'mean'
+    model = models[num-1]
+    var_name = vars[rank]
+    print("===========================================")
+
+    print(f"model {model} var {var_name} is doing")
+    composite(model,var_name=var_name)
+
+
+#%%
+
+def mean_same_number(num):
+    models = ['MPI_GE_onepct','MK36','GFDL_CM3','CanESM2','CESM1_CAM5','MPI_GE']
+    composite(models[num-1],reduction= 'mean_same_number')
+
+#%%
+# main run mean_all
+if __name__ == '__main__':
+
+    # === mpi4py ===
+    try:
+        from mpi4py import MPI
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+        npro = comm.Get_size()
+    except:
+        print('::: Warning: Proceeding without mpi4py! :::')
+        rank = 0
+        npro = 1
+
+    mean_same_number(num)
+
+# # %%
+# composite('MPI_GE_onepct_30','bDays',remove_ensmean=False)
+# # %%
+# composite('MPI_GE_onepct_30','bDays_ano',remove_ensmean=False)
+
+# # %%
+# composite('GFDL_CM3',reduction = 'mean_same_number')
+# # %%
---
+++ script/14calculations_paper/composite_analysis/2reanalysis_composite_boot.py
@@ -0,0 +1,179 @@
+# %%
+import xarray as xr
+import numpy as np
+import pandas as pd
+
+import src.composite.composite as composite
+import src.reanalysis.remove_force as rmf
+
+import mpi4py as MPI
+import glob
+import os
+import sys
+
+# %%
+import importlib
+
+importlib.reload(composite)
+
+
+# %%
+# === mpi4py ===
+try:
+    from mpi4py import MPI
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()  # 0,1,2,3,4,5,6,7,8,9
+    npro = comm.Get_size()  # 10
+except:
+    print("::: Warning: Proceeding without mpi4py! :::")
+    rank = 0
+    npro = 1
+
+# %%
+node = int(sys.argv[1])
+f1 = int(sys.argv[2])
+f2 = int(sys.argv[3])
+
+# %%
+#%%
+def time_convert(data):
+    data['time'] = pd.to_datetime(data['time'].values)
+    return data
+
+def read_temp_data(model,var_name = 't2max'):
+
+    odir = "/work/mh0033/m300883/Tel_MMLE/data/" + model + "/"
+    data_JJA = []
+    for month in ["Jun", "Jul", "Aug"]:
+        print(f"reading the gph data of {month} ...")
+        zg_path = odir + f"{var_name}_" + month + "/"
+        all_files = glob.glob(zg_path + "*.nc")
+        all_files.sort()
+
+        if model == 'ERA5' and var_name == 'ts':
+            data_month = xr.open_mfdataset(all_files,combine = 'by_coords',preprocess=time_convert)
+            try:
+                data_month = data_month['T2M']
+            except KeyError:
+                data_month = data_month['var167']
+        elif model == 'ERA5' and var_name == 't2max':
+            data_month = xr.open_dataset(all_files[0])
+            data_month = data_month['var167']
+        elif model == 'ERA5' and var_name == 't2min':
+            data_month = xr.open_dataset(all_files[0])
+            data_month = data_month['var167']
+        elif model == 'CR20':
+            data_month = xr.open_dataset(all_files[0])
+            data_month = data_month['air']
+        elif model == 'CR20_allens':
+            data_month = xr.open_mfdataset(all_files,combine = 'nested',concat_dim = 'ens')
+            data_month = data_month['TMP']
+
+        data_JJA.append(data_month)
+    data = xr.concat(data_JJA, dim="time").sortby("time")
+    data = data.sel(time = slice('1850','2015'))
+    data_inter = rmf.detrend(data, method="quadratic_trend")
+    return data_inter
+
+
+# %%
+def _select_data(data, extr_type, index):
+    period_data = data.sel(time=index.time, method="nearest")
+    period_data = period_data.sortby("time")
+    period_data["time"] = index.time
+
+
+    # get the coordinates of the extremes
+    extr_index = composite.extreme(index, extreme_type=extr_type, threshold=1.5)
+    extr_index.attrs["extreme_type"] = extr_type
+    extr_index = extr_index.sel(mode="NAO")
+
+    extr_index = extr_index.sortby("time")
+    sel_data = composite.sel_data(period_data, extr_index)
+    return sel_data
+
+def select_data(data,First_index,Last_index):
+    sel_first_pos = _select_data(data, "pos", First_index)
+    sel_first_neg = _select_data(data, "neg", First_index)
+
+    sel_last_pos = _select_data(data, "pos", Last_index)
+    sel_last_neg = _select_data(data, "neg", Last_index)
+
+    return sel_first_pos, sel_first_neg, sel_last_pos, sel_last_neg
+#%%
+def _resample_com_ind(sel_data, n_resamples=1000):
+    n_samples = sel_data.com.size
+    rng = np.random.default_rng(seed=12345)
+    sampled_index = rng.choice(n_samples, size=(n_samples, n_resamples), replace=True)
+    return sampled_index
+
+def resample_com_ind(sel_first_pos, sel_first_neg, sel_last_pos, sel_last_neg,n_resamples=1000):
+    res_first_pos = _resample_com_ind(sel_first_pos, n_resamples=n_resamples)
+    res_first_neg = _resample_com_ind(sel_first_neg, n_resamples=n_resamples)
+
+    res_last_pos = _resample_com_ind(sel_last_pos, n_resamples=n_resamples)
+    res_last_neg = _resample_com_ind(sel_last_neg, n_resamples=n_resamples)
+    return res_first_pos, res_first_neg, res_last_pos, res_last_neg
+
+def read_eof(model,group_size = 40):
+    odir = "/work/mh0033/m300883/Tel_MMLE/data/" + model + "/"
+    first_eof_path = odir + f"EOF_result/first_{str(group_size)}_eof_std.nc"
+    last_eof_path = odir + f"EOF_result/last_{str(group_size)}_eof_std.nc"
+
+    first_eof = xr.open_dataset(first_eof_path)
+    last_eof = xr.open_dataset(last_eof_path)
+    return first_eof.pc, last_eof.pc
+
+# %%
+model = "CR20_allens"
+# read gph data
+odir = "/work/mh0033/m300883/Tel_MMLE/data/" + model + "/"
+save_path = odir + "composite/"
+
+# %%
+data = read_temp_data(model,var_name = 'ts')
+First_index, Last_index = read_eof(model,group_size = 40)
+
+#%%
+n_resamples = 1000
+
+sel_first_pos, sel_first_neg, sel_last_pos, sel_last_neg = select_data(data,First_index,Last_index)
+
+res_first_pos, res_first_neg, res_last_pos, res_last_neg = resample_com_ind(sel_first_pos, sel_first_neg, sel_last_pos, sel_last_neg,n_resamples=n_resamples)
+
+# %%
+list_all_pros = [0] * npro
+for nn in range(npro):
+    list_all_pros[nn] = np.arange(f1,f2,1)[nn::npro]+rank
+steps = list_all_pros[rank]
+
+#%%
+for kk, step in enumerate(steps) :
+    print(f"node {node}: core:{rank} kk = {kk+1}/{steps.shape[0]}, step = {step}")
+
+    first_pos = sel_first_pos.isel(com=res_first_pos[:, step]).mean(dim = 'com')
+    first_neg = sel_first_neg.isel(com=res_first_neg[:, step]).mean(dim = 'com')
+
+    last_pos = sel_last_pos.isel(com=res_last_pos[:, step]).mean(dim = 'com')
+    last_neg = sel_last_neg.isel(com=res_last_neg[:, step]).mean(dim = 'com')
+    
+    num_info = step
+
+    print(f"saving files {num_info} ...")
+    first_pos.to_netcdf(
+        save_path + f"first_boot/composite_first_pos_{num_info}.nc"
+    )
+
+    first_neg.to_netcdf(
+        save_path + f"first_boot/composite_first_neg_{num_info}.nc"
+    )
+
+    last_pos.to_netcdf(
+        save_path + f"last_boot/composite_last_pos_{num_info}.nc"
+    )
+
+    last_neg.to_netcdf(
+        save_path + f"last_boot/composite_last_neg_{num_info}.nc"
+    )
+# %%
