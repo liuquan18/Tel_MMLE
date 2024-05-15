a4960544973162b1896a8c38b216e85e65fd0e08
Quan <quan.liu@mpimet.mpg.de>
2024-05-15 18:17:04 +0200

    start blocking index

--- script/calculations_paper/4diagnose_model/blocking_index.sh
+++ script/calculations_paper/4diagnose_model/blocking_index.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-source ~/.bashrc
+source /etc/profile
+
+
+# define function blocking_index
+blocking_index(){
+    # input arguments
+    model=$1
+    month=$2
+    # define path
+    path_in=/work/mh0033/m300883/Tel_MMLE/data/${model}/zg_${month}
+    path_out=${path}/${model}_${var}_${season}_${year}_${month}_${day}_blocking_index.nc
+    # calculate blocking index
+    cdo -s -b 32 -O -L setctomiss,0 -gtc,0 -setmissval,0 -setgrid,${path_in} -run,blocking_index,${path_in} ${path_out}
+}
+
+odir='/work/mh0033/m300883/Tel_MMLE/data/MPI_GE/zg_Aug
