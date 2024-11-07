8662b4043a0354793ee6fc53c9e4c72c9e93dce0
Quan <quan.liu@mpimet.mpg.de>
2024-11-07 11:21:39 +0100

    read jet stream

--- script/_explore_script/15mechnism/zonal_wind.py
+++ script/_explore_script/15mechnism/zonal_wind.py
@@ -2,31 +2,53 @@
 import xarray as xr
 import pandas as pd
 import seaborn as sns
+import glob
 # %%
 
-def read_extrc(model, fixed_pattern="decade_mpi"):
-    """read extreme counts"""
-    odir = "/work/mh0033/m300883/Tel_MMLE/data/" + model + "/extreme_count/"
-    filename = f"plev_50000_{fixed_pattern}_first_JJA_extre_counts.nc"
-    ds = xr.open_dataset(odir + filename).pc
+############## read NAO ###################
+def read_NAO_extremes( model, standard="first", season="JJA",vertical_eof = 'ind', plev = 50000):
+    eof_dir = (
+        f"/work/mh0033/m300883/Tel_MMLE/data/{model}/EOF_result/"
+        + f"troposphere_{vertical_eof}_decade_"
+        + standard
+        + "_"
+        + season
+        + "_eof_result.nc"
+    )
 
-    # divide the ensemble size of each model
-    ens_sizes = {
-        "MPI_GE": 100,
-        "MPI_GE_onepct": 100,
-        "CanESM2": 50,
-        "CESM1_CAM5": 40,
-        "MK36": 30,
-        "GFDL_CM3": 20,
-    }
-    ds = ds / ens_sizes[model]
-    count = ds.sel(confidence = 'true').sel(mode = 'NAO')
-    return count
+    eof_result = xr.open_dataset(eof_dir)
+
+    pc = eof_result.pc.sel(mode="NAO", plev=plev)
+
+    # extremes
+    pos = pc.where(pc > 1.5)
+    pos_df = pos.to_dataframe().reset_index()
+    pos_df = pos_df.dropna(subset=['pc'])
+
+    
+    neg = pc.where(pc < -1.5)
+    neg_df = neg.to_dataframe().reset_index()
+    neg_df = neg_df.dropna(subset=['pc'])
+
+    return pos_df,neg_df
+# %%
+NAO_pos, NAO_neg = read_NAO_extremes('MPI_GE')
 
 # %%
-extreme_counts = read_extrc('MPI_GE')
+def read_jetStream(model):
+    JetStream  = []
+    jet_dir = "/work/mh0033/m300883/Tel_MMLE/data/MPI_GE/NA_EJ_"
+    for month in ['Jun', 'Jul', 'Aug']:
+        all_ens_lists = sorted(glob.glob(jet_dir + month + "/*.nc"))
+        jet = xr.open_mfdataset(all_ens_lists, combine= 'nested', concat_dim = 'ens')
+        JetStream.append(jet)
+
+    JetStream = xr.concat(JetStream, dim = 'time')
+    # sort by time
+    JetStream = JetStream.sortby('time')
+    # exclude lon dim
+    JetStream = JetStream.isel(lon = 0)
+    return JetStream
 # %%
-NAO = extreme_counts.to_dataframe('count').reset_index()[['time','extr_type', 'count']]
-# %%
-uex = xr.open_dataset("/work/mh0033/m300883/Tel_MMLE/data/MPI_GE/u/hist_rcp85_0002_echam6_ATM_mm.nc")
+JetStream = read_jetStream('MPI_GE')
 # %%
