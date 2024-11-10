b278ba582902b81b486dae5988dc248ea5fdda34
Quan <quan.liu@mpimet.mpg.de>
2024-11-10 23:27:00 +0100

    done

--- script/_explore_script/15mechnism/jet_GB_together_evolution.py
+++ script/_explore_script/15mechnism/jet_GB_together_evolution.py
@@ -8,34 +8,10 @@ import src.composite.composite as composite
 import numpy as np
 import glob
 import scipy.stats as stats
-
+import matplotlib.pyplot as plt
 # %%
 from src.mechnisms.mechisms import *
-#%%
-def calculate_trend(data):
-    """
-    Calculate the temporal trend and significance interval of a time series.
 
-    Parameters:
-    data (xarray.DataArray): Input time series data with a time dimension.
-
-    Returns:
-    trend (float): The slope of the trend.
-    conf_interval (tuple): The 95% confidence interval of the trend.
-    """
-
-    # Get the time values as numbers
-    time_num = np.arange(len(data.time))
-
-    # standardize the data
-    data_std = (data - data.sel(time = slice("1850", "1900")).mean(dim = 'time')) / data.sel(time = slice("1850", "1900")).std(dim = 'time')
-    # Perform linear regression
-    slope, intercept, r_value, p_value, std_err = stats.linregress(time_num, data_std)
-
-    # Calculate the 95% confidence interval
-    conf_interval = stats.t.interval(0.95, len(time_num) - 2, loc=slope, scale=std_err)
-
-    return slope, conf_interval
 
 # %%
 model = "MPI_GE"
@@ -58,8 +34,6 @@ jet_loc_south.drop_vars("lon")
 # %%
 jet_north_decade = jet_loc_north.resample(time="10Y", closed = 'left').count().sum(dim = 'ens')
 
-# Calculate the trend and confidence interval for jet_north_decade
-jet_north_trend, jet_north_conf_interval = calculate_trend(jet_loc_north.resample(time="10Y", closed='left').count().sum(dim='ens'))
 
 # %%
 GB = read_greenland_blocking(model)
@@ -77,10 +51,22 @@ GB_below = GB_below.drop_vars(["lon", "lat", "plev"])
 # %%
 GB_above_decade = GB_above.resample(time="10Y", closed = 'left').count().sum(dim = 'ens')
 GB_below_decade = GB_below.resample(time="10Y", closed = 'left').count().sum(dim = 'ens')
+# %%
+def decade_corr(x,y):
+    x = x.stack(com = ('time','ens'))
+    y = y.stack(com = ('time','ens'))
+    corr = xr.corr(x, y, dim='com')
+    return corr
+# %%
+jet_GB_corr = jet_loc.resample(time = '10Y',closed="left" ).apply(
+    lambda x: decade_corr(x, GB.sel(time = x.time))
+)
+# %%
+jet_GB_corr = jet_GB_corr.drop_vars(('lon', 'lat', 'plev'))
 
 # %%
-fig = plt.figure(figsize=(6, 6))
-gs = fig.add_gridspec(2, 1)
+fig = plt.figure(figsize=(6, 8))
+gs = fig.add_gridspec(3, 1)
 
 # Left panel for time series
 ax1 = fig.add_subplot(gs[0])
@@ -104,27 +90,34 @@ ax1.set_ylabel('Latitude')
 ax_twin1.set_ylabel('count')
 ax1.set_title('Eddy-driven jet stream location')
 
-ax3 = fig.add_subplot(gs[1])
-GB_clim.plot(ax=ax3, label='Climatology', x='time', add_legend=False, color='black')
+ax2 = fig.add_subplot(gs[1])
+GB_clim.plot(ax=ax2, label='Climatology', x='time', add_legend=False, color='black')
 # clean the tile of ax
-ax3.set_title('')
+ax2.set_title('')
 
-ax3.fill_between(GB_clim.time, GB_clim - GB_std, GB_clim + GB_std, color='gray', alpha=0.5, label='std')
-ax3.set_ylim(5.45, 5.7)
-ax3.grid(False)  # Remove gridlines
-ax_twin2 = ax3.twinx()
+ax2.fill_between(GB_clim.time, GB_clim - GB_std, GB_clim + GB_std, color='gray', alpha=0.5, label='std')
+ax2.set_ylim(5.45, 5.7)
+ax2.grid(False)  # Remove gridlines
+ax_twin2 = ax2.twinx()
 GB_above_decade.plot(ax=ax_twin2, color='red', label='count of GB above 1.5 std')
 ax_twin2.grid(False)  # Remove gridlines
 
-lines, labels = ax3.get_legend_handles_labels()
+lines, labels = ax2.get_legend_handles_labels()
 lines2, labels2 = ax_twin2.get_legend_handles_labels()
-ax3.legend(lines + lines2, labels + labels2, loc='upper left', frameon=False)
+ax2.legend(lines + lines2, labels + labels2, loc='upper left', frameon=False)
 
-ax3.set_ylabel('GB proxy (km)')
+ax2.set_ylabel('GB proxy (km)')
 ax_twin2.set_ylabel('count')
-ax3.set_title('Greenland Blocking Index')
+ax2.set_title('Greenland Blocking Index')
 
+ax3 = fig.add_subplot(gs[2])
+jet_GB_corr.plot(ax=ax3, color = 'k')
+ax3.set_ylabel("Correlation")
+ax3.set_title("Jet Stream location and Blocking correlation")
+# without grids
+ax3.grid(False)
 
 plt.tight_layout()
+
 plt.savefig("/work/mh0033/m300883/Tel_MMLE/docs/source/plots/mechism/jet_GB_index_evolve.png")
 # %%
