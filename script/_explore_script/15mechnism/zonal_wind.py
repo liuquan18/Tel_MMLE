6a72973b87b53fb1673a93bc59d51fc62e7d0248
Quan <quan.liu@mpimet.mpg.de>
2024-11-15 14:47:09 +0100

    for all models

--- script/_explore_script/15mechnism/zonal_wind.py
+++ script/_explore_script/15mechnism/zonal_wind.py
@@ -4,6 +4,7 @@ import pandas as pd
 import seaborn as sns
 import glob
 import matplotlib.pyplot as plt
+import os
 from src.mechnisms.mechisms import read_jetStream, Jet_location
 # %%
 
@@ -61,48 +62,48 @@ def decade_jet_NS(jet_loc, jet_loc_clim, jet_loc_std, scale = 1.5, fix_clim = Fa
     return jet_loc_north, jet_loc_south
 
 # %%
-jet_stream = read_jetStream("MPI_GE")
-jet_stream = jet_stream.compute()
-# %%
-jet_loc = Jet_location(jet_stream)
-# %%
-jet_loc_clim = Jet_climatology(jet_loc)
-jet_loc_clim = jet_loc_clim.drop_vars('lon')
-#%%
-jet_loc_std = Jet_climatology(jet_loc, stat = 'std')
-jet_loc_std = jet_loc_std.drop_vars('lon')
+def zonal_wind_process(model):
+    jet_stream = read_jetStream(model)
+    jet_stream = jet_stream.compute()
+
+    jet_loc = Jet_location(jet_stream)
+
+    jet_loc_clim = Jet_climatology(jet_loc)
+    jet_loc_clim = jet_loc_clim.drop_vars('lon')
+
+    jet_loc_std = Jet_climatology(jet_loc, stat = 'std')
+    jet_loc_std = jet_loc_std.drop_vars('lon')
+
+
+
+    jet_loc_north, jet_loc_south = decade_jet_NS(jet_loc, jet_loc_clim, jet_loc_std, fix_clim=True)
+
+
+    jet_loc_north.name = 'jet_loc'
+    jet_loc_south.name = 'jet_loc'
+
+    jet_north_decade = jet_loc_north.resample(time="10Y", closed = 'left').count().sum(dim = 'ens')
+
+
+    # save results
+    clim_to_dir = f"/work/mh0033/m300883/Tel_MMLE/data/{model}/mechnisms/"
+
+    # create dir if not exist
+    if not os.path.exists(clim_to_dir):
+        os.makedirs(clim_to_dir)
+
+    jet_loc_clim.to_netcdf(clim_to_dir + 'jet_loc_clim.nc')
+    jet_loc_std.to_netcdf(clim_to_dir + 'jet_loc_std.nc')
+
+    jet_north_decade.to_netcdf(clim_to_dir + 'jet_north_decade.nc')
+
+    return jet_loc_clim, jet_loc_std, jet_north_decade
+
 
 # %%
-jet_loc_north, jet_loc_south = decade_jet_NS(jet_loc, jet_loc_clim, jet_loc_std, fix_clim=True)
+jet_loc_clim, jet_loc_std, jet_north_decade = zonal_wind_process('MPI_GE')
 
-#%%
-jet_loc_north.name = 'jet_loc'
-jet_loc_south.name = 'jet_loc'
 
-jet_loc_north.to_netcdf('/work/mh0033/m300883/Tel_MMLE/data/MPI_GE/mechnisms/jet_loc_north.nc')
-jet_loc_south.to_netcdf('/work/mh0033/m300883/Tel_MMLE/data/MPI_GE/mechnisms/jet_loc_south.nc')
-# %%
-# above 1 std clim
-# %%
-jet_north_decade = jet_loc_north.resample(time="10Y", closed = 'left').count().sum(dim = 'ens')
-# %%
-jet_south_decade = jet_loc_south.resample(time="10Y", closed = 'left').count().sum(dim = 'ens')
-
-#%%
-fig, ax = plt.subplots()
-jet_loc_clim.plot(ax=ax)
-# jet_stream.isel(ens = 0).plot(x = 'time', ax=ax)
-# fill between the std
-ax.fill_between(jet_loc_clim.time, jet_loc_clim - jet_loc_std, jet_loc_clim + jet_loc_std, color='gray', alpha=0.5)
-jet_loc_north.plot.scatter(x = 'time', ax = ax)
-jet_loc_south.plot.scatter(x = 'time', ax = ax)
-# %%
-jet_north_decade.plot()
-# %%
-jet_south_decade.plot()
-#%%
-
-# %%
 fig, ax = plt.subplots()
 jet_loc_clim.plot(ax=ax, label='Climatology',x = 'time', add_legend = False)
 # clean the tile of ax
@@ -112,7 +113,7 @@ ax.fill_between(jet_loc_clim.time, jet_loc_clim - jet_loc_std, jet_loc_clim + je
 ax.set_ylim(42,57)
 ax_twin = ax.twinx()
 jet_north_decade.drop_vars('lon').plot(ax = ax_twin, color = 'red', label = 'count of jet Norther than 1 std')
-ax_twin.set_ylim(-800, 1000)
+# ax_twin.set_ylim(-800, 1000)
 
 lines, labels = ax.get_legend_handles_labels()
 lines2, labels2 = ax_twin.get_legend_handles_labels()
@@ -121,24 +122,9 @@ ax.legend(lines + lines2, labels + labels2, loc='upper left', frameon=False)
 ax.set_ylabel('Latitude')
 ax_twin.set_ylabel('count')
 ax.set_title('Eddy-driven jet stream location')
-plt.savefig("/work/mh0033/m300883/Tel_MMLE/docs/source/plots/mechism/jet_stream.png", dpi = 300)
+# plt.savefig("/work/mh0033/m300883/Tel_MMLE/docs/source/plots/mechism/jet_stream.png", dpi = 300)
 
 # %%
-jet_loc_clim_perc = (jet_loc_clim - jet_loc_clim.isel(time=0))/jet_loc_clim.isel(time=0) * 100
-jet_loc_upper = jet_loc_clim + jet_loc_std
-jet_loc_lower = jet_loc_clim - jet_loc_std
-
-jet_loc_upper_perc = (jet_loc_upper - jet_loc_upper.isel(time=0))/jet_loc_upper.isel(time=0) * 100
-jet_loc_lower_perc = (jet_loc_lower - jet_loc_lower.isel(time=0))/jet_loc_lower.isel(time=0) * 100
-
-#%%
-fig, ax = plt.subplots()
-jet_loc_clim_perc.plot(ax=ax, label='Climatology')
-ax.fill_between(jet_loc_clim_perc.time, jet_loc_lower_perc, jet_loc_upper_perc, color='gray', alpha=0.5, label = 'std')
-
-ax.set_ylabel('Latitude anomaly (%)')
-ax.set_title('eddy-driven Jet stream location')
-
-plt.savefig("/work/mh0033/m300883/Tel_MMLE/docs/source/plots/mechism/jet_stream_anomaly.png", dpi = 300)
-
-#%%
+for model in ["CanESM2", "CESM1_CAM5", "GFDL_CM3", "MK36"]:
+    zonal_wind_process(model)
+# %%
