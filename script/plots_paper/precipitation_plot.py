ae10a7b1c12972c705e7b633a366fb1e3e2ac240
Quan <quan.liu@mpimet.mpg.de>
2025-02-21 10:13:06 +0100

    get the pltos

---
+++ script/plots_paper/precipitation_plot.py
@@ -0,0 +1,187 @@
+# %%
+import xarray as xr
+import numpy as np
+import src.plots.utils as utils
+
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import matplotlib as mpl
+import proplot as pplt
+from matplotlib.lines import Line2D
+
+from matplotlib.ticker import MaxNLocator
+import matplotlib.patches as mpatches
+
+
+import src.plots.composite_plot as composite_plot
+import src.plots.extreme_plot as extplt
+import src.plots.statistical_overview as stat_overview
+import src.obs.era5_extreme_change as era5_extreme_change
+import src.extreme.extreme_count_troposphere as ext_profile
+
+#%%
+def read_composite(
+    model, var_name, fixed_pattern="decade_mpi", reduction="mean_same_number"
+):
+    """read composite data"""
+    odir = "/work/mh0033/m300883/Tel_MMLE/data/"
+    comp_name = f"plev_50000_{fixed_pattern}_first_JJA_JJA_first_last_{var_name}_composite_{reduction}.nc"
+    composite = xr.open_dataset(f"{odir}{model}/composite/{comp_name}")
+    if var_name == "ts":
+        try:
+            composite = composite.tsurf
+        except AttributeError:
+            composite = composite.ts
+    elif var_name == "pr":
+        try:
+            composite = composite.pr
+        except AttributeError:
+            composite = composite.precip
+    return composite
+
+
+# %%
+models = ["MPI_GE", "CanESM2", "CESM1_CAM5", "MK36", "GFDL_CM3"]
+models_legend = [
+    "MPI-GE (100)",
+    "CanESM2 (50)",
+    "CESM1-CAM5 (40)",
+    "MK3.6 (30)",
+    "GFDL-CM3 (20)",
+]
+
+COMPOSITEs = {
+    model: read_composite(model, var_name="pr", reduction='mean') for model in models
+}
+#%%
+# change units from kg m-2 s-1 to mm/day
+for model in models:
+    COMPOSITEs[model] = COMPOSITEs[model] * 86400
+# %%
+#%%
+cmap_new =  plt.matplotlib.colors.LinearSegmentedColormap.from_list('rr_colors',
+                                        ['white', '#1ca8e9','#097cc4', '#095389',
+                                         'forestgreen','yellowgreen',  'gold','orangered',
+                                         'brown', 'black'], gamma = 1.)
+
+# %%
+
+def plot_composite_single_ext(COMPOSITEs, models, axes, extr_type="pos", **kwargs):
+    levels = kwargs.get("levels", np.arange(-1.2, 1.3, 0.3))
+    for i, model in enumerate(models):  # cols for different models
+        first = COMPOSITEs[model].sel(mode="NAO", period="first", extr_type=extr_type)
+        last = COMPOSITEs[model].sel(mode="NAO", period="last", extr_type=extr_type)
+        diff = COMPOSITEs[model].sel(mode="NAO", period="diff", extr_type=extr_type)
+        diff_sig = COMPOSITEs[model].sel(
+            mode="NAO", period="diff_sig", extr_type=extr_type
+        )
+        try:
+            first = utils.erase_white_line(first)
+            last = utils.erase_white_line(last)
+            diff = utils.erase_white_line(diff)
+            diff_sig = utils.erase_white_line(diff_sig)
+        except ValueError:
+            pass
+
+        data_all = [
+            first,
+            last,
+            diff,
+        ]
+
+        maps = []
+        for j, data in enumerate(data_all):  # row for different data
+            map = axes[j, i].contourf(
+                data,
+                x="lon",
+                y="lat",
+                levels=levels,
+                extend="both",
+                transform=ccrs.PlateCarree(),
+                cmap = cmap_new,
+            )
+            maps.append(map)
+            axes[j, i].grid(color="grey7", linewidth=0.5)
+
+        # significant area as hatches.
+        if i < len(models) - 1:
+            axes[2, i].contourf(
+                diff_sig,
+                levels=[-0.5, 0.5, 1.5],
+                colors=["none", "none"],
+                hatches=["", "xxxxx"],
+            )
+
+    return axes, maps
+
+# %%
+
+fig3, axes = pplt.subplots(
+    space=0,
+    width=180 / 25.4,
+    wspace=0.2,
+    hspace=0.2,
+    proj="ortho",
+    proj_kw=({"lon_0": -20, "lat_0": 60}),
+    nrows=3,
+    ncols=5,
+)
+axes.format(
+    latlines=20,
+    lonlines=30,
+    color="grey7",
+    coast=True,
+    coastlinewidth=0.3,
+    coastcolor="charcoal",
+    leftlabels=["first", "last", "last - first"],
+    toplabels=models_legend,
+    toplabels_kw={"fontsize": 7, },
+    leftlabels_kw={"fontsize": 7,},
+)
+
+
+axes, maps = composite_plot.plot_composite_single_ext(COMPOSITEs, models, axes)
+fig3.colorbar(
+    maps[0],
+    loc="b",
+    pad=1,
+    title="precipitation (mm/day)",
+    width=0.1,
+    shrink=1,
+)
+
+# %%
+fig4, axes = pplt.subplots(
+    space=0,
+    width=180 / 25.4,
+    wspace=0.2,
+    hspace=0.2,
+    proj="ortho",
+    proj_kw=({"lon_0": -20, "lat_0": 60}),
+    nrows=3,
+    ncols=5,
+)
+axes.format(
+    latlines=20,
+    lonlines=30,
+    color="grey7",
+    coast=True,
+    coastlinewidth=0.3,
+    coastcolor="charcoal",
+    leftlabels=["first", "last", "last - first"],
+    toplabels=models_legend,
+    toplabels_kw={"fontsize": 7, },
+    leftlabels_kw={"fontsize": 7,},
+)
+
+
+axes, maps = composite_plot.plot_composite_single_ext(COMPOSITEs, models, axes, 'neg')
+fig4.colorbar(
+    maps[0],
+    loc="b",
+    pad=1,
+    title="precipitation (mm/day)",
+    width=0.1,
+    shrink=1,
+)
+# %%
