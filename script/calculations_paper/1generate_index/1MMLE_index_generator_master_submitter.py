7d572b55e3a9050ce8844ba31e06c04eccc3f840
Quan <quan.liu@mpimet.mpg.de>
2025-02-22 21:10:33 +0100

    name

--- script/calculations_paper/1generate_index/1MMLE_index_generator_master_submitter.py
+++ script/calculations_paper/1generate_index/1MMLE_index_generator_master_submitter.py
@@ -12,5 +12,5 @@ njobs  = int(nsteps/npar) # 2
 for kk in range(njobs): #0,1
   k1 = kk*npar #0,4,8
   k2 = (kk+1)*npar #4,8,12
-  os.system(f"sbatch ./index_generator_submitter.sh {kk+1} {k1} {k2}") # 1,0,4, 2,4,8, 3,8,12
+  os.system(f"sbatch ./1MMLE_index_generator_submitter.sh {kk+1} {k1} {k2}") # 1,0,4, 2,4,8, 3,8,12
 # %%
