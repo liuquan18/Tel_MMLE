65fc400790b961255bd058275047525bee42da8d
Quan <quan.liu@mpimet.mpg.de>
2025-02-26 13:35:22 +0100

    row columns

--- script/plots_paper/precipitation_plot.py
+++ script/plots_paper/precipitation_plot.py
@@ -260,3 +260,151 @@ fig4.colorbar(
 
 plt.savefig("/work/mh0033/m300883/Tel_MMLE/docs/source/plots/paper_supplymentary/precipitation_composite_neg.pdf", dpi=300, bbox_inches="tight")
 # %%
+
+# %%
+def plot_composite_single_ext_rc(COMPOSITEs, models, axes, extr_type="pos", fill = True, **kwargs):
+    levels_shading = kwargs.get("levels_shading", np.arange(-1.2, 1.3, 0.3))
+    levels_lines = kwargs.get("levels_lines", np.arange(-2, 2.1, 0.5))
+    for i, model in enumerate(models):  # rows for different models
+        first = COMPOSITEs[model].sel(mode="NAO", period="first", extr_type=extr_type)
+        last = COMPOSITEs[model].sel(mode="NAO", period="last", extr_type=extr_type)
+        diff = COMPOSITEs[model].sel(mode="NAO", period="diff", extr_type=extr_type)
+
+        try:
+            first = utils.erase_white_line(first)
+            last = utils.erase_white_line(last)
+            diff = utils.erase_white_line(diff)
+        except ValueError:
+            pass
+
+        data_all = [
+            first,
+            last,
+            diff,
+        ]
+
+        maps = []
+        for j, data in enumerate(data_all):  # cols for different data
+            if fill is True:
+                map = axes[i, j].contourf(
+                    data,
+                    x="lon",
+                    y="lat",
+                    levels=levels_shading,
+                    extend="both",
+                    transform=ccrs.PlateCarree(),
+                    cmap = prec_cmap_div,
+                )
+            else:
+                map = axes[i, j].contour(
+                    data,
+                    x="lon",
+                    y="lat",
+                    levels=[level for level in levels_lines if level != 0],
+                    colors = 'k',
+                    linewidths = 0.5,
+                    extend="both",
+                    transform=ccrs.PlateCarree(),
+                    zorder = 10,
+                )
+            maps.append(map)
+            axes[i, j].grid(color="grey7", linewidth=0.5)
+
+        # significant area as hatches.
+        if fill is True:
+            if i < len(models) - 1:
+                diff_sig = COMPOSITEs[model].sel(
+                    mode="NAO", period="diff_sig", extr_type=extr_type
+                )
+
+                diff_sig = utils.erase_white_line(diff_sig)
+
+                axes[i, 2].contourf(
+                    diff_sig,
+                    levels=[-0.5, 0.5, 1.5],
+                    colors=["none", "none"],
+                    hatches=["", "xxxxx"],
+                    zorder=20,
+                )
+
+    return axes, maps
+
+# %%
+
+fig3, axes = pplt.subplots(
+    space=0,
+    width=180 / 25.4,
+    wspace=0.2,
+    hspace=0.2,
+    proj="ortho",
+    proj_kw=({"lon_0": -20, "lat_0": 60}),
+    nrows=6,
+    ncols=3,
+)
+axes.format(
+    abc = True,
+    latlines=20,
+    lonlines=30,
+    color="grey7",
+    coast=True,
+    coastlinewidth=0.3,
+    coastcolor="charcoal",
+    leftlabels=models_legend,
+    toplabels=["first", "last", "last - first"],
+    toplabels_kw={"fontsize": 7, },
+    leftlabels_kw={"fontsize": 7,},
+)
+
+
+axes, maps = plot_composite_single_ext_rc(COMPOSITEs_pr, models_plot, axes)
+axes, lines = plot_composite_single_ext_rc(COMPOSITEs_psl, models_plot, axes, fill=False, levels_lines=np.arange(-5, 5.1, 1))
+
+fig3.colorbar(
+    maps[0],
+    loc="b",
+    pad=1,
+    title="precipitation (mm/day)",
+    width=0.1,
+    shrink=1,
+)
+
+plt.savefig("/work/mh0033/m300883/Tel_MMLE/docs/source/plots/paper_supplymentary/precipitation_composite_pos_rc.pdf", dpi=300, bbox_inches="tight")
+
+# %%
+fig4, axes = pplt.subplots(
+    space=0,
+    abc=True,
+    width=180 / 25.4,
+    wspace=0.2,
+    hspace=0.2,
+    proj="ortho",
+    proj_kw=({"lon_0": -20, "lat_0": 60}),
+    nrows=6,
+    ncols=3,
+)
+axes.format(
+    latlines=20,
+    lonlines=30,
+    color="grey7",
+    coast=True,
+    coastlinewidth=0.3,
+    coastcolor="charcoal",
+    leftlabels=models_legend,
+    toplabels=["first", "last", "last - first"],
+    toplabels_kw={"fontsize": 7,},
+    leftlabels_kw={"fontsize": 7,},
+)
+
+axes, maps = plot_composite_single_ext_rc(COMPOSITEs_pr, models_plot, axes, 'neg')
+axes, lines = plot_composite_single_ext_rc(COMPOSITEs_psl, models_plot, axes, 'neg', fill=False, levels_lines=np.arange(-5, 5.1, 1))
+fig4.colorbar(
+    maps[0],
+    loc="b",
+    pad=1,
+    title="precipitation (mm/day)",
+    width=0.1,
+    shrink=1,
+)
+
+plt.savefig("/work/mh0033/m300883/Tel_MMLE/docs/source/plots/paper_supplymentary/precipitation_composite_neg_rc.pdf", dpi=300, bbox_inches="tight")
+# %%
