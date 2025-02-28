33769e733803daca7c812958dacfca1dd1285bc1
Quan <quan.liu@mpimet.mpg.de>
2025-02-28 09:19:12 +0100

    path

--- script/_explore_script/18revison/var_mean_first_last.sh
+++ script/_explore_script/18revison/var_mean_first_last.sh
@@ -12,13 +12,13 @@
 module load cdo/2.5.0-gcc-11.2.0
 var=$1
 odir=/work/mh0033/m300883/Tel_MMLE/data/MPI_GE/$var/
-todir=/work/mh0033/m300883/Tel_MMLE/data/MPI_GE/${var}_first_last/
+todir=/work/mh0033/m300883/Tel_MMLE/data/MPI_GE/zg_first_last/
 files=($odir/*.nc)
 
 
 # first 1850-1859
 
-# cdo -P 32 -timmean -sellevel,50000 -ensmean -apply,selyear,1850/1859 [ ${files[@]} ] ${todir}${var}_1850-1859.nc
+cdo -P 32 -timmean -sellevel,50000 -ensmean -apply,selyear,1850/1859 [ ${files[@]} ] ${todir}${var}_1850-1859.nc
 
 # last 2090-2099
 
