dd43dcc98a114136c39d6cc39b363b082881c106
Quan <quan.liu@mpimet.mpg.de>
2025-03-25 14:10:50 +0100

    get plot

--- script/_explore_script/18revison/box_based_index.py
+++ script/_explore_script/18revison/box_based_index.py
@@ -4,7 +4,12 @@ import numpy as np
 import pandas as pd
 import glob
 import warnings
+import matplotlib.pyplot as plt
+from scipy.stats import linregress
+
+#%%
 from src.MMLE_TEL.index_stats import decadal_extrc
+
 #%%
 
 def read_data(
@@ -66,6 +71,27 @@ def read_data(
         zg_plev = zg_demean # for the data only with one plev
     return zg_plev
 
+
+def plot_extreme_counts(extrc, ax, extr_type, model_color, ci = True):
+    
+    line = extrc.sel(extr_type=extr_type,confidence = 'true').plot.line(
+            ax=ax, 
+            label='box_based',
+            x = 'time',
+            color = 'k',
+            linewidth = 2,)
+
+    if ci:
+        # fill between the confidence interval ['low','high']
+        ax.fill_between(
+            extrc.time,
+            extrc.sel(extr_type=extr_type,confidence = 'low').values,
+            extrc.sel(extr_type=extr_type,confidence = 'high').values,
+            color = 'gray',
+            alpha = 0.3,
+        )
+
+    return line
 # %%
 def box_diff(zg):
     south_box = zg.sel(lat = slice(55, 45), lon = slice(-25, 5)).mean(dim = ('lat', 'lon'))
@@ -92,13 +118,66 @@ NAO_first = NAO_index.sel(time = slice('1850','1859'))
 mean = NAO_first.mean()
 std = NAO_first.std()
 
-NAO_last = NAO_index.sel(time = slice('2090','2099'))
 # %%
 NAO_index_ano = (NAO_index - mean)/std
+#%%
+NAO_ano_first = NAO_index_ano.sel(time = slice('1850','1859'))
+NAO_ano_last = NAO_index_ano.sel(time = slice('2090','2099'))
 # %%
 NAO_index_ano.to_netcdf("/work/mh0033/m300883/Tel_MMLE/data/MPI_GE/box_diff/NAO_index_ano.nc")
 # %%
 extrc = decadal_extrc(NAO_index_ano)
 # %%
 extrc.to_netcdf("/work/mh0033/m300883/Tel_MMLE/data/MPI_GE/box_diff/NAO_index_ano_extre_counts.nc")
+# %%
+true_extrc = extrc.sel(confidence = 'true')
+pos = true_extrc.sel(extr_type = 'pos')
+neg = true_extrc.sel(extr_type = 'neg')
+# %%
+# calculate the p-value of trend in pos and neg
+
+def trend_pval(da):
+    # calculate the trend
+    x = np.arange(len(da.time))
+    trend = linregress(x, da.values)
+    return trend.pvalue
+p_value_pos = trend_pval(pos)
+p_value_neg = trend_pval(neg)
+
+#%%
+# plot the distribution of the NAO index
+fig, axes = plt.subplots(1, 3, figsize=(12, 5))
+NAO_ano_first.plot.hist(ax=axes[0], alpha=0.7, bins=np.arange(-4, 4.1, 0.5), color='k', label='first10')
+NAO_ano_last.plot.hist(ax=axes[0], alpha=0.7, bins=np.arange(-4, 4.1, 0.5), color='r', label='last10')
+
+# vline at mean
+axes[0].axvline(NAO_ano_first.mean(), color='k', linestyle='dashed', linewidth=1.5)
+axes[0].axvline(NAO_ano_last.mean(), color='r', linestyle='dashed', linewidth=1.5)
+
+# Remove right and top spines
+for ax in axes:
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+pos_line = plot_extreme_counts(extrc, axes[1], 'pos', 'k')
+neg_line = plot_extreme_counts(extrc, axes[2], 'neg', 'k')
+
+axes[1].set_ylabel('extreme counts')
+axes[2].set_ylabel('extreme counts')
+
+axes[0].set_title('')
+axes[1].set_title('')
+axes[2].set_title('')
+axes[0].set_xlabel('NAO index')
+# add a, b,c
+axes[0].text(0.05, 0.95, 'a', transform=axes[0].transAxes, fontsize=16, fontweight='bold', va='top')
+axes[1].text(0.05, 0.95, 'b', transform=axes[1].transAxes, fontsize=16, fontweight='bold', va='top')
+axes[2].text(0.05, 0.95, 'c', transform=axes[2].transAxes, fontsize=16, fontweight='bold', va='top')
+
+# add the p-value of trend
+axes[1].text(0.25, 0.95, f"p-value: {p_value_pos:.2f}", transform=axes[1].transAxes, fontsize=10, va='top')
+axes[2].text(0.25, 0.95, f"p-value: {p_value_neg:.2f}", transform=axes[2].transAxes, fontsize=10, va='top')
+
+plt.tight_layout()
+plt.savefig("/work/mh0033/m300883/Tel_MMLE/docs/source/plots/paper_supplymentary/box_diff_NAO_index.pdf", bbox_inches='tight')
 # %%
